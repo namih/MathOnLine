8e6f17e140a30778984728e4a54f0f62d022396d
krmncc <carmen.chavez.conde@gmail.com>
2017-05-17 17:46:32 -0500

    Maquetación de la vista de registro

---
+++ application/views/Registro_usuario_v.php
@@ -0,0 +1,68 @@
+<div class="conteiner-fluid
+">
+	<h3>REGISTRO</h3>
+	<h5>*Campos Obligatorios</h5>
+	<div class="row">
+		<div class="input-group col-lg-12 col-md-12 col-xs-12">
+			<div class="form-group col-md-6  col-xs-12">
+				<input type="text" class="form-control" id="usr" placeholder="Nombre de Usuario*">
+			</div>
+		</div>
+		<!-- <p></p>
+		<p></p> -->
+		<div class="form-group col-md-6  col-xs-12">
+			<input type="password" class="form-control" id="pwd" placeholder="Contraseña*">
+		</div>
+		<div class="form-group col-md-6  col-xs-12">
+			<input type="password" class="form-control" id="pwd" placeholder="Repetir Contraseña*">
+		</div>
+		
+		<div class="input-group col-lg-12 col-md-12 col-xs-12">
+			<!-- <p></p>
+			<p></p> -->
+			<div class="form-group col-md-6  col-xs-12">
+				<input type="email" class="form-control" id="correo" placeholder="Correo Electrónico*">
+			</div>
+		</div>
+		<!-- <p></p>
+		<p></p> -->
+		<div class="form-group col-md-6  col-xs-12">
+			<input type="text" class="form-control" id="name" placeholder="Nombre(s)">
+		</div>
+		<!-- <p></p>
+		<p></p> -->
+		<div class="form-group col-md-6  col-xs-12">
+			<input type="text" class="form-control" id="last" placeholder="Apellidos">
+		</div>
+		<!-- <p></p>
+		<p></p> -->
+		<div class="form-group col-md-6  col-xs-12">
+			<input type="text" class="form-control" id="anio" placeholder="Año de Nacimiento*">
+		</div>
+		<div class="form-group col-md-6 col-xs-12">
+					<select class="form-control" >
+						<option>Sexo</option>
+						<option>Hombre</option>
+						<option>Mujer</option>
+						
+					</select>
+				</div>
+				<div class="form-group col-md-6 col-xs-12">
+					<select class="form-control" >
+						<option>Unidad UAM</option>
+						
+						
+					</select>
+				</div>
+				<div class="form-group col-md-6  col-xs-12">
+					<input type="text" class="form-control" id="mat" placeholder="Matrícula" disabled>
+				</div>
+				<div class="checkbox col-md-6  col-xs-12">
+      				<label><input type="checkbox" value="" checked >Estudia</label>
+    			</div>
+    			<div class="checkbox col-md-6  col-xs-12">
+      				<label><input type="checkbox" value="">Trabaja</label>
+    			</div>
+	</div>
+		
+</div>
