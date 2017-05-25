1cd36e0ce301b5a51bfafbff149b3dc165857d57
Cesar Padilla <sisbyte@Mac-mini-de-LDCBI.local>
2017-05-25 12:40:16 -0500

    Layout para activacion de cuenta y sql para agregar temas del mes dummies

--- application/views/email/activacion_cuenta.php
+++ application/views/email/activacion_cuenta.php
@@ -9,13 +9,12 @@
 			</div>
 			<div idid="body">
 				<div>
-					<h2 style="font-family:Helvetica">Solicitud de recuperación de contraseña</h2><br>
+					<h2 style="font-family:Helvetica">Activación para su cuenta de Mate en Línea</h2><br>
 					<p align="justify" style="font-family:Helvetica">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 				<div>
 					<br />
-					<p align="left" style="font-family:Helvetica">Nombre de usuario: <strong><?php echo $user_name ?></strong> </p>
-					<p align="left" style="font-family:Helvetica">Su contraseña es: <strong><?php echo $password ?></strong> </p>
+					<h1>AQUI VA LA "LIGA DE ACTIVACION"</h1>
 					<br />
 					<p align="justify" style="font-family:Helvetica">Este correo es informativo, favor no responder a esta dirección de correo, ya que no se encuentra habilitada para recibir mensajes.</p>
 				</div>
