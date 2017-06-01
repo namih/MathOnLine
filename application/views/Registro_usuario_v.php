07cd04dc222c9e1272dac5009c41bcf1170e4e46
krmncc <carmen.chavez.conde@gmail.com>
2017-06-01 15:28:55 -0500

    Validación de nombre de usuario y correo electrónico. Se añadió el campo de licenciatura.

--- application/views/Registro_usuario_v.php
+++ application/views/Registro_usuario_v.php
@@ -1,47 +1,55 @@
-
+<style>.has-feedback .form-control-feedback {
+            position: absolute;
+            top: 0px;
+            right: 15px;
+            display: block;
+            width: 34px;
+            height: 34px;
+            line-height: 34px;
+            text-align: center;
+        }</style>
 <div class="container">
 	<h3>REGISTRO</h3>
 	<h5>*Campos Obligatorios</h5>
 	<div class="row">
 		<div class="input-group col-lg-12 col-md-12 col-xs-12">
-			<div class="form-group col-md-6  col-xs-12">
-				<input type="text" class="form-control" id="usr" placeholder="Nombre de Usuario*">
+			<div class="form-group has-feedback col-md-6  col-xs-12" id="usuario">
+				<input onblur="validar_username()" type="text" class="form-control" id="usr" placeholder="Nombre de Usuario*">
+				<span id="error_username" class="glyphicon glyphicon-remove form-control-feedback" style="display: none"></span>
 			</div>
 		</div>
-		<!-- <p></p>
-		<p></p> -->
+		
 		<div class="form-group col-md-6  col-xs-12">
-			<input type="password" class="form-control" id="pwd" placeholder="Contraseña*">
+			<input type="password" class="form-control" id="pwd" placeholder="Contraseña*" required>
+			
 		</div>
 		<div class="form-group col-md-6  col-xs-12">
 			<input type="password" class="form-control" id="rpwd" placeholder="Repetir Contraseña*">
 		</div>
 		
+		
+			
 		<div class="input-group col-lg-12 col-md-12 col-xs-12">
-			<!-- <p></p>
-			<p></p> -->
-			<div class="form-group col-md-6  col-xs-12">
-				<input type="email" class="form-control" id="correo" placeholder="Correo Electrónico*">
-			</div>
-			<div class="form-group col-md-6  col-xs-12 " style="display: none" id="msj_correo">
-				<span class="help-inline" style="color: red">Correo inválido.</span>
+			<div class="form-group has-feedback col-md-6  col-xs-12" id="email">
+				<input onblur="validar_correo()" type="email" class="form-control" id="correo" placeholder="Correo Electrónico*" requiered>
+				<span id="error_email" class="glyphicon glyphicon-remove form-control-feedback" style="display: none"></span>
 			</div>
 		</div>
+			
+			
+		
 
-		<!-- <p></p>
-		<p></p> -->
+		
 		<div class="form-group col-md-6  col-xs-12">
 			<input type="text" class="form-control" id="name" placeholder="Nombre(s)">
 		</div>
-		<!-- <p></p>
-		<p></p> -->
+		
 		<div class="form-group col-md-6  col-xs-12">
 			<input type="text" class="form-control" id="last" placeholder="Apellidos">
 		</div>
-		<!-- <p></p>
-		<p></p> -->
+		
 		<div class="form-group col-md-6  col-xs-12">
-			<input type="text" class="form-control" id="anio" placeholder="Año de Nacimiento*">
+			<input type="text" class="form-control" id="anio" placeholder="Año de Nacimiento*" required="">
 		</div>
 		<div class="form-group col-md-6 col-xs-12">
 					<select class="form-control" id="sexo">
@@ -52,12 +60,18 @@
 					</select>
 				</div>
 				<div class="form-group col-md-6 col-xs-12">
-					<select onchange="seleccion()" class="form-control" id ="uam">
+					<select onchange="seleccionar_unidad()" class="form-control" id ="uam">
 						<option value="0">Unidad UAM</option>
 						<?php 
 						for ($i=0; $i < count($unidades) ; $i++) { ?>
 							<option value="<?php echo $unidades[$i]['id_unit_uam']; ?>"><?php echo $unidades[$i]['name']?></option>
 						<?php }?>
+					</select>
+				</div>
+				<div class="form-group col-md-6 col-xs-12">
+					<select  class="form-control" id ="carrera" disabled>
+						<option value="0">Licenciatura</option>
+						
 					</select>
 				</div>
 				<div class="form-group col-md-6  col-xs-12">
@@ -68,12 +82,11 @@
       				<label class="col-md-6  col-xs-6"><input type="checkbox" value="" id="trabaja">Trabaja</label>
     			</div>
     			<div class="input-group col-lg-12 col-md-12 col-xs-12">
-			<!-- <p></p>
-			<p></p> -->
-			<div class="form-group col-md-6  col-xs-12">
-				<button type="button" onclick="registrar()" class="btn btn-primary">Registrar</button>
-				<button type="button" class="btn btn-danger">Cancelar</button>
-			</div>
+			
+					<div class="form-group col-md-6  col-xs-12">
+						<button type="button" onclick="registrar()" class="btn btn-primary">Registrar</button>
+						<button type="button" class="btn btn-danger">Cancelar</button>
+					</div>
 		</div>
 
 	</div>
--- statics/js/registro_usuario/Registro_usuario.js
+++ statics/js/registro_usuario/Registro_usuario.js
@@ -1,18 +1,17 @@
-/**document.getElementById('msj_correo').style.display='inline';
-		 document.getElementById('correo').style.borderColor = "red";
-* La función password verifica que las contraseñas sean del mismo tamaño así como
-* los mismos caracteres
+/**
+* La función seleccion permite escoger la unidad uam a la que pertenece y con la elección se auto-chequeala opción "estudia",
+* se habilita la caja de texto correspondiente a "Matricula" y de acorde a la unidad uam seleccionada en el desplegable de carreras se muestran las que son impartidas en la unidad.
 *
 * @author María del Carmen Chávez Conde
 * @param NA
-* @return TRUE cuando los strings coinciden, FALSE si los strings no coinciden. Formato: [* @return tipo comentario]
+* @return  Formato: [* @return tipo comentario]
 * @version Versión actual del elemento
 */
 
 function seleccion () {
   	var unidades = document.getElementById('uam').value;
-	var matricula = document.getElementById('mat');
-	var estudia = document.getElementById('estudia');
+	var matricula = document.getElementById('carrera');
+	
 	if (unidades == 0) {
 		matricula.disabled = true;
 		matricula.value = '';
@@ -28,6 +27,58 @@ function seleccion () {
 	};
 }
 
+function seleccionar_unidad () {
+	var unidades = document.getElementById('uam').value;
+	var matricula = document.getElementById('mat');
+	var estudia = document.getElementById('estudia');
+	var licenciatura = document.getElementById('carrera');
+	if (unidades == 0) {
+		matricula.disabled = true;
+		licenciatura.disabled = true;
+		matricula.value = '';
+		estudia.checked = false;
+	} else if(unidades == 1){
+		
+		matricula.disabled = true;
+		licenciatura.disabled = true;
+		matricula.value = '';
+		estudia.checked = false;
+	}else{
+		matricula.disabled = false;
+		licenciatura.disabled = false;
+		estudia.checked = true;
+	};
+	if(unidades != 0){
+		var datos = {'unidad':unidades};
+		$.ajax({
+		type:'post',
+		url: "http://localhost/MathOnLine/index.php/Registro_usuario_c/registrar_usuario",
+		data: {datos:datos},
+		datatype: 'json',
+		cache: false,
+		success: function() {
+			var carreras = document.getElementById('carrera');
+			var option = document.createElement('option');
+			option.text = 'hola';
+			carreras.add(option);
+			
+		},
+		error: function() {
+			alert('failure');
+		}
+	});
+	}
+  
+}
+/**
+* La función comparar_contrasenia verifica que las contraseñas sean del mismo tamaño así como
+* los mismos caracteres
+*
+* @author María del Carmen Chávez Conde
+* @param NA
+* @return TRUE cuando los strings coinciden, FALSE si los strings no coinciden. Formato: [* @return tipo comentario]
+* @version Versión actual del elemento
+*/
 
 function comparar_contrasenia () {
 	var contraseña1 = document.getElementById('pwd').value;
@@ -39,6 +90,15 @@ function comparar_contrasenia () {
 		return false;
 	};
 }
+
+/**
+* La función validar_email verifica que el correo electrónico con el que se registró el usuario
+* este escrito con la estructura "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/".
+*@author María del Carmen Chávez Conde
+* @param NA
+* @return TRUE cuando el correo electrónico esta escrito correctamente, FALSE cuando el correo electrónico no esta escrito correctamente. Formato: [* @return tipo comentario]
+* @version Versión actual del elemento
+*/
 
 function validar_email() {
 	var valor = document.getElementById('correo');
@@ -53,6 +113,13 @@ function validar_email() {
 	}
 }
 
+/**
+* La función registrar manda al controlador Registro_usuario_c los datos que ingresó la persona que desea registrarse
+*@author María del Carmen Chávez Conde
+* @param NA
+* @return. Formato: [* @return tipo comentario]
+* @version Versión actual del elemento
+*/
 function registrar () {
   var usuario = document.getElementById('usr').value;
   var contrasenia1 = document.getElementById('pwd').value;
@@ -101,11 +168,85 @@ function registrar () {
 	} else{
 		alert('error');
 	};
-	
-	
-	
-	
-	
-	
-	
 }
+/**
+* La función registrar manda al controlador Registro_usuario_c los datos que ingresó la persona que desea registrarse
+*@author María del Carmen Chávez Conde
+* @param NA
+* @return. Formato: [* @return tipo comentario]
+* @version Versión actual del elemento
+*/
+function validar_username() {
+	document.getElementById('usr').style.borderColor = '';
+    document.getElementById("error_username").style.display= 'none';
+	
+	var user = document.getElementById('usr').value;
+	var datos = {user_name:user};
+  	if(user != ''){
+  		$.ajax({
+  			type:'post',
+					url: "http://localhost/MathOnLine/index.php/Registro_usuario_c/nombre_usuario_disponible",
+					data: {datos:datos},
+					datatype: 'json',
+					cache: false,
+					success: function(msj) {
+						if (msj == 'SI') {
+							document.getElementById('usr').style.borderColor = 'green';
+						} else{
+							var usuario = document.getElementById("usuario");
+							usuario.className += " has-error";
+							document.getElementById("error_username").style.display= 'inline';
+							document.getElementById('usr').value = '';
+							document.getElementById('usr').placeholder = 'Nombre de usuario no disponible';
+						};
+					},
+					error: function(msj) {
+						alert('Failure: '+msj);
+					}
+				});
+  			
+  		
+  	}
+}
+
+/**
+* La función registrar manda al controlador Registro_usuario_c los datos que ingresó la persona que desea registrarse
+*@author María del Carmen Chávez Conde
+* @param NA
+* @return. Formato: [* @return tipo comentario]
+* @version Versión actual del elemento
+*/
+
+function validar_correo () {
+	document.getElementById('correo').style.borderColor = '';
+    document.getElementById("error_email").style.display= 'none';
+	
+	var user = document.getElementById('correo').value;
+	var datos = {email:user};
+  	if(user != ''){
+  		$.ajax({
+  			type:'post',
+					url: "http://localhost/MathOnLine/index.php/Registro_usuario_c/correo_usuario_disponible",
+					data: {datos:datos},
+					datatype: 'json',
+					cache: false,
+					success: function(msj) {
+						if (msj == 'SI') {
+							document.getElementById('usr').style.borderColor = 'green';
+						} else{
+							var usuario = document.getElementById("email");
+							usuario.className += " has-error";
+							document.getElementById("error_email").style.display= 'inline';
+							document.getElementById('correo').value = '';
+							document.getElementById('correo').placeholder = 'Correo no disponible';
+						};
+					},
+					error: function(msj) {
+						alert('Failure: '+msj);
+					}
+				});
+  			
+  		
+  	}
+}
+  
