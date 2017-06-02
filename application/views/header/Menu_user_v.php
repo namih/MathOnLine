0f28c72903ad305a2c6647c9a40c9596a376a637
mimis <fatal.experience@gmail.com>
2017-06-02 17:53:47 -0500

    menu usuario

---
+++ application/views/header/Menu_user_v.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" href="statics/css/style_user.css">
+<div  class="col-md-12 example3" id="nav">
+  <nav class="navbar navbar-inverse navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo base_url(); ?>"><img src="statics/img/logo_matenlinea/mate1.jpg" alt="Dispute Bills">
+        </a>
+      </div>
+      <div id="navbar3" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Contact</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li class="dropdown-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+          <li class="dropdown" id="log-user">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img width="35px" src="statics/img/iniciar_session/people-145129_1280.png"/></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Consultar perfil</a></li>
+              <li><a href="#">Cerrar sesión</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <div class="col-md-12">
+        <hr style="height: 1px; border: 0; color: #666; background-color: #008c00;">
+    </div>
+    <!--/.container-fluid -->
+  </nav>
+</div>
