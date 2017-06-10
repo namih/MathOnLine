35cd666fc7f3360e1fd2e9ded5d4f1ffda30246d
namih <fatal.experience@gmail.com>
2017-06-10 13:20:00 -0500

    eliminacion de codigo comentado

--- application/views/inicio/Home_v.php
+++ application/views/inicio/Home_v.php
@@ -1,58 +1,3 @@
-<?php /*$themes=array(
-    0 => array        (
-            'id_monthly_theme' => '5',
-            'id_user' => '1',
-            'mounth' => '5',
-            'title' => '5 trucos para mejorar tus matemáticas',
-            'description' => 'Para muchos adultos las matemáticas son esa ‘asignatura pendiente’, y no digamos para muchos estudiantes que se enfrentan con ansiedad y desilusión a los temidos exámenes.
-Contar con los dedos, no comprender cómo resolver problemas aritméticos, no recordar las reglas y procedimientos, equivocarse en las cuentas… son algunos de los problemas que pueden encontrarse. Te proporcionamos algunos trucos para intentar que las matemáticas no se te atraganten o que, si se te dan bien, puedan ser tu mejor habilidad.
-1. Mejora tus habilidades espaciales: Yi-Ling Cheng y Kelly S. Mix han publicado recientemente (2014) que el entrenamiento en una tarea de ‘rotación mental’ de objetos mejoraba la capacidad de cálculo de niños entre 6 y 8 años. Una sola sesión y el grupo de entrenamiento mejoró significativamente en operaciones de completar huecos, como por ejemplo 8 + __ =12.
-2. Gesticula cuando analices las instrucciones: Un nuevo artículo aparecido en la revista Psychological Science (2014) explica cómo los niños que fueron entrenados en hacer gestos para representar ideas abstractas contenidas en problemas matemáticos aprendieron mejor a resolver esos problemas y otros distintos, generalizando dicho aprendizaje.
-3. Entrena tu memoria de trabajo y tus funciones ejecutivas: El trabajo publicado en el Journal of Educational Psychology en 2014 muestra cómo la memoria de trabajo es importante no sólo para explicar algunas dificultades con las matemáticas como por ejemplo resolver problemas escritos, sino también para sacar beneficio de un entrenamiento dedicado a mejorar las estrategias de los participantes.
-4. Controla la ansiedad ante las matemáticas: Según relatan Sharifah N. S. Wahid y sus colaboradores (2014), el rendimiento matemático de un grupo de estudiantes universitarios era menor cuanto mayor era su nivel de ansiedad ante las matemáticas. Los autores destacan la importancia de motivar a los estudiantes y desarrollar su autoconfianza en relación a esta materia.
-5. Juega al ajedrez: Parece ser, según una reciente revisión científica publicada en Frontiers in Psychology (2014) que jugar al ajedrez podría ser una buena forma de estimular algunas de esas funciones mentales que también se ponen en marcha en las tareas matemáticas, como la comprensión, la capacidad de análisis o el razonamiento inductivo.
-Sabemos que no es fácil pero también que no es imposible ser mejor en matemáticas, así que ponte en marcha y sobre todo, no pierdas los nervios en el camino.',
-            'image' => '/statics/tema_mes/imagen_5.png',
-            'url_video' => '',
-            'status' => '1',
-            'mes' => 'Mayo'
-        ),
-    1 => array(
-            'id_monthly_theme' => '6',
-            'id_user' => '1',
-            'mounth' => '6',
-            'title' => 'Diez curiosidades científicas e históricas sobre el número 12',
-            'description' => 'Recopilamos algunos datos curiosos sobre el número doce:
-1. El doce ha estado ligado durante milenios a la medición del tiempo. Tanto calendarios como relojes se organizan en base a esta cifra: el año consta de 12 meses y el día de 24 horas. Cada hora se divide en 5 × 12 minutos, y cada minuto en 5 × 12 segundos.
-2. El duodeno (derivado del término de origen latino "duodecim", que significa "doce") es parte del intestino delgado y en un adulto mide aproximadamente 12 pulgadas de longitud (30,5 cm).
-3. La graduación usual de la circunferencia es 360º, es decir, 12 × 30º.
-4. También está omnipresente en ciertas unidades de medida. Un pie (unidad de longitud de origen natural) tiene 12 pulgadas.
-5. El cuerpo humano cuenta con 12 pares craneales, un grupo de nervios con funciones sensitivas, motoras o mixtas que emergen por los agujeros del cráneo y se relacionan, sobre todo, con la cabeza y el cuello.
-6. El doce es el número más pequeño con seis divisores: 1, 2, 3, 4, 6 y 12. Así, el número doce y sus múltiplos son los únicos que tienen a la vez exactamente mitad, tercio, cuarto, dos tercios y tres cuartos.
-7. En la mitología y la religión, la importancia de esta cifra es notable. Para los griegos, los dioses olímpicos del Panteón eran doce, como también fueron una docena los trabajos encargados al mítico héroe romano Hércules como penitencia. Según la Biblia, Jacob tuvo doce hijos, y en este mismo libro se menciona que hubo doce tribus de Israel y doce apóstoles.
-8. El número de teclas de función de la mayoría de los teclados de ordenador es 12 (F1 a F12).
-9. El escritor norteamericano F. Emerson Andrews fundó el 5 de abril de 1944 The Duodecimal Society con el objetivo de investigar y educar "en la ciencia matemática, con especial dedicación al uso de la Base Doce de numeración, en matemáticas, pesos y medidas". En su publicación periódica, bautizada como The Duodecimal Bulletin, difundían las virtudes de este sistema de numeración.
-10. Un total de 12 astronautas han caminado hasta ahora sobre la Luna. Los primeros pisaron el satélite en 1969 y los últimos astronautas pusieron un pie en la superficie lunar en 1972. Todos ellos formaron parte de alguna de las seis misiones Apolo de la NASA.',
-            'image' => '/statics/tema_mes/imagen_6.png',
-            'url_video' => '',
-            'status' => '1',
-            'mes' => 'Junio'
-        ),
-    2 => array(
-            'id_monthly_theme' => '7',
-            'id_user' => '1',
-            'mounth' => '7',
-            'title' => 'Los espermatozoides hacen cálculos matemáticos muy complejos',
-            'description' => 'Los cambios en la concentración de calcio dentro de los espermatozoides controlan a qué velocidad baten estas células sus colas (flagelos) para navegar en dirección al óvulo, su destino final. Y estos cambios en los niveles de calcio se producen, precisamente, cuando los óvulos secretan determinadas sustancias. Científicos del Instituto Max Planck de Alemania, con la participación del investigador español Luis Álvarez, acaban de demostrar que durante este proceso los espermatozoides realizan cálculos muy complejos, que implican "medir las tasas de cambio a lo largo del tiempo, es decir, cómo de rápido o de lento cambia la concentración de calcio que entra en la célula". Expresado en lenguaje matemático, podría decirse que los espermatozoides no miden valores absolutos, sino que calculan la derivada temporal de la concentración de calcio.
-La investigación se ha centrado en analizar espermatozoides de diferentes especies marinas usando luz láser estroboscópica -similar a la que se utiliza en las discotecas-. Sin embargo, los científicos sospechan que el esperma de otras especies, incluidos los seres humanos, podría comportarse de la misma manera.
-Además, ahora que se ha demostrado que "no hace falta todo un cerebro para hacer operaciones matemáticas", es posible que la extraordinaria capacidad de cálculo detectada en los espermatozoides se aplique a otros mensajeros químicos intracelulares que controlan diferentes funciones vitales en distintas células del organismo. "Esto es solo el principio", adelantan los autores de la investigación.',
-            'image' => '/statics/tema_mes/imagen_7.png',
-            'url_video' =>'',
-            'status' => '1',
-            'mes' => 'Julio'
-        )
-);*/
-?>
     <div id="contentInicio">
       <div class="col-md-12"><br>
         <div class="row">
@@ -86,48 +31,7 @@ Además, ahora que se ha demostrado que "no hace falta todo un cerebro para hace
                       </div>
                     </div>
                 <?php } }?>
-                <!--<div class="item active">
-                  <div class="panel panel-success tema_mes">
-                    <div class="panel-heading" align="center">Mayo</div>
-                    <div class="panel-body">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque bibendum felis, eget tristique libero dapibus eu. Curabitur vitae fermentum lorem. Donec porttitor velit non tortor luctus, faucibus efficitur odio lobortis. Proin nisl nulla, aliquam ac blandit at, commodo semper ipsum. Fusce dignissim, quam quis finibus consequat, libero libero hendrerit nisl, et finibus arcu enim a risus. Morbi a enim nec justo finibus pellentesque. Fusce imperdiet tortor eu sodales scelerisque. Nunc gravida dui id magna finibus accumsan. Phasellus iaculis elit sit amet mauris elementum, sed suscipit justo aliquet. Nam ultricies vitae urna sit amet sagittis.<br><br>
-
-                    Nulla gravida luctus dui quis tempus. Nunc maximus orci vitae est porttitor, in mattis mi semper. Integer hendrerit porta erat id ultricies. Aenean a turpis id urna vestibulum sollicitudin. Donec eget leo interdum, hendrerit nunc ornare, sollicitudin massa. Donec felis tortor, faucibus non quam id, sollicitudin maximus risus. Nunc arcu lectus, scelerisque sit amet nisl a, mattis placerat libero. Phasellus sagittis dictum diam eu sollicitudin. Vestibulum vehicula in nibh sed gravida. Vivamus vestibulum sed tellus a tempus. Quisque fermentum odio vitae nisl pharetra fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consequat tincidunt facilisis. Curabitur quis libero luctus, efficitur sapien ut, finibus lacus. Nullam ut sem magna. Pellentesque vulputate, diam a dictum suscipit, magna lacus posuere massa, vehicula dignissim elit libero tristique ex.
-                    <br><br>
-
-                    Nulla gravida luctus dui quis tempus. Nunc maximus orci vitae est porttitor, in mattis mi semper. Integer hendrerit porta erat id ultricies. Aenean a turpis id urna vestibulum sollicitudin. Donec eget leo interdum, hendrerit nunc ornare, sollicitudin massa. Donec felis tortor, faucibus non quam id, sollicitudin maximus risus. Nunc arcu lectus, scelerisque sit amet nisl a, mattis placerat libero. Phasellus sagittis dictum diam eu sollicitudin. Vestibulum vehicula in nibh sed gravida. Vivamus vestibulum sed tellus a tempus. Quisque fermentum odio vitae nisl pharetra fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consequat tincidunt facilisis. Curabitur quis libero luctus, efficitur sapien ut, finibus lacus. Nullam ut sem magna. Pellentesque vulputate, diam a dictum suscipit, magna lacus posuere massa, vehicula dignissim elit libero tristique ex.
-
-                    <br><br>
-
-                    Nulla gravida luctus dui quis tempus. Nunc maximus orci vitae est porttitor, in mattis mi semper. Integer hendrerit porta erat id ultricies. Aenean a turpis id urna vestibulum sollicitudin. Donec eget leo interdum, hendrerit nunc ornare, sollicitudin massa. Donec felis tortor, faucibus non quam id, sollicitudin maximus risus. Nunc arcu lectus, scelerisque sit amet nisl a, mattis placerat libero. Phasellus sagittis dictum diam eu sollicitudin. Vestibulum vehicula in nibh sed gravida. Vivamus vestibulum sed tellus a tempus. Quisque fermentum odio vitae nisl pharetra fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consequat tincidunt facilisis. Curabitur quis libero luctus, efficitur sapien ut, finibus lacus. Nullam ut sem magna. Pellentesque vulputate, diam a dictum suscipit, magna lacus posuere massa, vehicula dignissim elit libero tristique ex.
-
-                    <br><br>
-
-                    Nulla gravida luctus dui quis tempus. Nunc maximus orci vitae est porttitor, in mattis mi semper. Integer hendrerit porta erat id ultricies. Aenean a turpis id urna vestibulum sollicitudin. Donec eget leo interdum, hendrerit nunc ornare, sollicitudin massa. Donec felis tortor, faucibus non quam id, sollicitudin maximus risus. Nunc arcu lectus, scelerisque sit amet nisl a, mattis placerat libero. Phasellus sagittis dictum diam eu sollicitudin. Vestibulum vehicula in nibh sed gravida. Vivamus vestibulum sed tellus a tempus. Quisque fermentum odio vitae nisl pharetra fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consequat tincidunt facilisis. Curabitur quis libero luctus, efficitur sapien ut, finibus lacus. Nullam ut sem magna. Pellentesque vulputate, diam a dictum suscipit, magna lacus posuere massa, vehicula dignissim elit libero tristique ex.
-                    </div>
-                  </div> 
-                </div>
-
-                <div class="item">
-                  <div class="panel panel-success tema_mes">
-                    <div class="panel-heading" align="center">Junio</div>
-                    <div class="panel-body">
-                    Nulla gravida luctus dui quis tempus. Nunc maximus orci vitae est porttitor, in mattis mi semper. Integer hendrerit porta erat id ultricies. Aenean a turpis id urna vestibulum sollicitudin. Donec eget leo interdum, hendrerit nunc ornare, sollicitudin massa. Donec felis tortor, faucibus non quam id, sollicitudin maximus risus. Nunc arcu lectus, scelerisque sit amet nisl a, mattis placerat libero. Phasellus sagittis dictum diam eu sollicitudin. Vestibulum vehicula in nibh sed gravida. Vivamus vestibulum sed tellus a tempus. Quisque fermentum odio vitae nisl pharetra fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec consequat tincidunt facilisis. Curabitur quis libero luctus, efficitur sapien ut, finibus lacus. Nullam ut sem magna. Pellentesque vulputate, diam a dictum suscipit, magna lacus posuere massa, vehicula dignissim elit libero tristique ex.
-                    </div>
-                  </div> 
-                </div>-->
-
               </div>
-
-              <!-- Left and right controls -->
-              <!--<a class="left carousel-control" href="#temaMes" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Anterior</span>
-              </a>
-              <a class="right carousel-control" href="#temaMes" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Siguiente</span>
-              </a>-->
             </div>                     
           <?php } ?>
           </div>
