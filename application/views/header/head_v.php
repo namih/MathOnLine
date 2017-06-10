7222fdf5da699e78b7e688722f6e4c9e47ad812a
namih <fatal.experience@gmail.com>
2017-06-10 11:20:16 -0500

    pruebas2

--- application/views/header/head_v.php
+++ application/views/header/head_v.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mate en línea</title>
-    <link rel="stylesheet" href="statics/css/bootstrap-3.3.7/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>statics/css/bootstrap-3.3.7/dist/css/bootstrap.css">
 
-    <link rel="stylesheet" href="statics/css/styles.css">
-    <script src="statics/js/jquery-3.2.1.min.js"></script>
-    <script src="statics/css/bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>statics/css/styles.css">
+    <script src="<?php echo base_url(); ?>statics/js/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>statics/css/bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
   </head> 
   <body>
     <script>var base_url ='<?php echo base_url();?>';</script>
