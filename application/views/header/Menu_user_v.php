8bf82e2a3f1eb8ac89f15bc3f2e87af745a328db
mimis <fatal.experience@gmail.com>
2017-06-14 14:26:44 -0500

    modificaciones al contenido de la sesion del usuario

--- application/views/header/Menu_user_v.php
+++ application/views/header/Menu_user_v.php
@@ -1,4 +1,4 @@
-<div class="col-md-12">  
+<!--<div class="col-md-12">  
   <nav class="navbar navbar-green" style=" margin-left: 3px !important; margin-right: 3px !important;">
     <div class="container-fluid">
       <ul class="nav navbar-nav navbar-right">
@@ -8,7 +8,7 @@
               <li><a href="#">Material complementario</a></li>
               <li><a href="#">Estadísticas</a></li>
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"> </span> Mimis</a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"> </span></a>
                 <ul class="dropdown-menu" role="menu">
                   <li><a href="#"><span class="glyphicon glyphicon-user"> </span> Consultar perfil</a></li>
                   <li><a href="#"><span class="glyphicon glyphicon-log-in"> </span> Cerrar sesión</a></li>
@@ -16,5 +16,37 @@
               </li>
     </ul>
     </div>
+  </div>-->
+<div class="col-md-12">  
   </nav>
-</div> 
+      <nav class="navbar navbar-green">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <!--<a class="navbar-brand visible-xs" href="#">Mate en linea</a>-->
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="#">Inicio</a></li>
+              <li><a href="#">Tutoriales</a></li>
+              <li><a href="#">Pruebas de evaluación</a></li>
+              <li><a href="#">Material complementario</a></li>
+              <li><a href="#">Estadísticas</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"> </span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#"><span class="glyphicon glyphicon-user"> </span> Consultar perfil</a></li>
+                  <li><a href="#"><span class="glyphicon glyphicon-log-in"> </span> Cerrar sesión</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav> 
+</div>
+    <?php //echo '<pre>'; print_r($user_log); print_r($temas); ?>
