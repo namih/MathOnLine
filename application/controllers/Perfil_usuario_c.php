f57ebcf585695c9c286ad486b67f12e879a7702e
mimis <fatal.experience@gmail.com>
2017-06-28 14:13:02 -0500

    ajustes al controlador para regresar los datos requeridos

--- application/controllers/Perfil_usuario_c.php
+++ application/controllers/Perfil_usuario_c.php
@@ -8,14 +8,21 @@
 		parent::__construct();
 		$this->load->model('Perfil_usuario_m');
     }
-	public function perfil_usuario($id_user = null)
-	{
+	public function perfil_usuario($id_user = null){
+
+		$datos=array();
 		if ($id_user != null){
 			$perfil = $this->Perfil_usuario_m->datos_usuario($id_user);
-			return $perfil;
+			$datos['user_log']=$perfil;/*Aqui solo los datos del usuario, la lista de avatares disponibles que sea separada por favor*/
 		} else {
-			return false;
+			$datos['user_log']= false;
 		}
+
+		$datos['avatar_list'] = array();/*Aqui colocar lo que regrese la consulta de avatares de acuerdo al genero del usuario logueado*/
+
+		$this->load->view('usuarios/Perfil_v', $datos);/* lo que se regresa debe ser la vista no el arreglo con los datos*/
 	}
-    }
+
+
+}/* END controller*/
 ?>
