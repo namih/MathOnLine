c00e96765c82b79e952bd49393902755f4e2e656
mimis <fatal.experience@gmail.com>
2017-07-27 13:41:56 -0500

    imagen en ek titulo del tema

--- application/views/inicio/Home_v.php
+++ application/views/inicio/Home_v.php
@@ -23,8 +23,10 @@
                       <div class="panel panel-success tema_mes"  align="center">
                         <div class="panel-heading" align="center"><?php echo $value['mes']; ?></div>
                         <div class="panel-body">
-                          <h3><?php echo $value['title']; ?> </h3><br><br>
-                          <img class="img-responsive" style="max-height: 200px; max-width: 80%; " src="<?php echo base_url().$value['image']; ?>"/>
+                          <div id="titulo" style="background-image: url('<?php echo base_url().$value['image']; ?>'); background-repeat: no-repeat; background-size: cover; opacity: 0.8;"><br>
+                            <h3 style="background-color: #000000; opacity: 0.7; padding: 12px; color: #FFFFFF; font-weight: bold;"><?php echo $value['title']; ?> </h3><br><br>
+                            <!--<img class="img-responsive" style="max-height: 200px; max-width: 80%; " src="<?php echo base_url().$value['image']; ?>"/>-->
+                          </div>
                           <br><br>
                           <p style="text-transform: capitalize; text-indent: 20px;">
                             <?php echo $value['description']; ?> 
