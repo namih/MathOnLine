6ed0322d69ccfa550d244157bbc69559cefcbcac
krmncc <carmen.chavez.conde@gmail.com>
2017-07-27 17:37:13 -0500

    login_admin_v-27/07/2017

--- application/views/administrador/Login_admin_v.php
+++ application/views/administrador/Login_admin_v.php
@@ -13,7 +13,11 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		
+		
+		<!-- Subir múltiples archivos     -->
+		<script src="../statics/js/jquery.MultiFile.js/"></script>    
 
 	</head>
 
@@ -24,7 +28,7 @@
 		<div class="container">
 			<div class="col-md-6 col-xs-12">
 				<h2>Tema del mes.</h2>
-				<table class="table table-striped">
+				<table class="table ">
     				<thead>
       					<tr>
         					<th>Tema</th>
@@ -36,10 +40,24 @@
       					
     				</tbody>
     					<?php for ($i=0;$i < count($temas);$i++){ ?>
-    						
+    						<tr>
+    							<td> <?php echo $temas[$i]['title'] ?></td>
+    							<td> <?php echo $temas[$i]['mounth'] ?></td>
+    							<td> <a href="#"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> </a> </td>
+    						</tr>
     					<?php }
 					?>
   				</table>
+			</div>
+			<div class="col-md-6 col-xs-12">
+				<h2>Calendario Escolar</h2>
+				<img class="img-responsive" src="../statics/img/calendario_uam/calendario_uam.png"/>
+				<form method="post" enctype="multipart/form-data">
+					<label for="file">Nombre del Archivo:</label>
+            		<input type="file" name="file1" id="file1" /> 
+            		<br />
+            		<input type="submit" name="subir" value="Subir" />
+        		</form>
 			</div>	
 		</div>
 	</body>
