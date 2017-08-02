5057f2c5ba106c2447fd47995577f446fa40ecb5
mimis <fatal.experience@gmail.com>
2017-08-02 16:23:42 -0500

    cambio en controlador para cargar la evaluacion de un tema

--- application/views/menu_usuario/Panel_evaluaciones_v.php
+++ application/views/menu_usuario/Panel_evaluaciones_v.php
@@ -11,7 +11,7 @@
                       <?php if(isset($temas) && !empty($temas)){ 
                         foreach ($temas as $key => $value) { ?>
                           <div class="col-lg-6">
-                            <a href="#" onclick="loadOptionMenu('<?php echo base_url() ?>Aux_mimi_c/carga_evaluacion_tema/<?php echo $value['id_tema']; ?>');">
+                            <a href="#" onclick="loadOptionMenu('<?php echo base_url() ?>Evaluation_c/get_evaluation/<?php echo $value['id_tema']; ?>');">
                               <div class="panel panel-info">
                                 <div class="panel-heading" style="background-color: #ffffff !important; min-height: 200px; padding: 40px; ">
                                       <center>
