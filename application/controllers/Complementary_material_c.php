40c9df3c83f27431abd2a64cc368c74473bef5aa
osvaldo172 <osvaldo172@gmail.com>
2017-08-05 11:29:58 -0500

    material complementario

--- application/controllers/Complementary_material_c.php
+++ application/controllers/Complementary_material_c.php
@@ -11,12 +11,21 @@ class Complementary_material_c  extends CI_Controller
     }
 
     public function index(){
-       $datos['bibliografia'] = $this ->bibliografia();
-	   $datos['link'] = $this ->links();
-	   //$this->load->model('Complementary_material_v',$datos);
-	   $this->load->view('header/head_v');
-		$this->load->view('/usuarios/Material_complementario_v',$datos);
-    	$this->load->view('footer/footer_v');
+    	$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){
+	        $datos['bibliografia'] = $this ->bibliografia();
+		    $datos['link'] = $this ->links();
+		    //$this->load->model('Complementary_material_v',$datos);
+		    $datos["user_log"][0] = $this->session->userdata('user');
+		    $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+	        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+		    $this->load->view('header/head_v');
+		    $this->load->view('header/Menu_user_v', $datos);
+			$this->load->view('/usuarios/Material_complementario_v',$datos);
+	    	$this->load->view('footer/footer_v');
+	    }else{
+	    	redirect(base_url());
+	    }
     }
 	
 	/**
