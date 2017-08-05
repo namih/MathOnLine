0a8792523e19aa91394845e643f5895273e9c0c5
LcHer <ceciferch@gmail.com>
2017-08-05 12:42:50 -0500

    meses

--- application/libraries/etiquetas.php
+++ application/libraries/etiquetas.php
@@ -10,6 +10,23 @@ class Etiquetas{
 		return $type;
 	}
 	
+	function meses(){
+	$mes=array(
+			1 => 'Enero',
+			2 => 'Febrero',
+			3 => 'Marzo',
+			4 => 'Abril',
+			5 => 'Mayo',
+			6 => 'Junio',
+			7 => 'Julio',
+			8 => 'Agosto',
+			9 => 'Septiembre',
+			10 => 'Octubre',
+			11 => 'Noviembre',
+			12 => 'Diciembre');
+		return $mes;	
+}
+	
 	function type_avatar()
 	{
 		$type = array(
@@ -173,21 +190,6 @@ class Etiquetas{
 }
 
 
-function meses(){
-	$mes=array(
-			1 => 'Enero',
-			2 => 'Febrero',
-			3 => 'Marzo',
-			4 => 'Abril',
-			5 => 'Mayo',
-			6 => 'Junio',
-			7 => 'Julio',
-			8 => 'Agosto',
-			9 => 'Septiembre',
-			10 => 'Octubre',
-			11 => 'Noviembre',
-			12 => 'Diciembre');
-		return $mes;	
-}
+
 
 ?>
