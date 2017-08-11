e33a040a602200e85cc9a01d8f09f3d24fc6cff9
krmncc <carmen.chavez.conde@gmail.com>
2017-08-11 18:53:45 -0500

    11 de agosto 2017

--- application/views/administrador/Editor_tema_mes_v.php
+++ application/views/administrador/Editor_tema_mes_v.php
@@ -5,27 +5,41 @@
 <!-- Lenguaje español -->
 <script src="<?php echo base_url(); ?>statics/js/summernote/lang/summernote-es-ES.js"></script>
 <script src="<?php echo base_url(); ?>statics/js/Login_admin/editor_summernote.js"></script>
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">  
-  	<hr style="height: 2px; border: 0; color: #666; background-color: #008c00; margin-top: 0px !important;  margin-bottom: 0px !important; ">
-</div> 
 
-<div class="container-fluid">
-	<div class="col-md-12">
-		<div class="col-md-12">
-			<div class="form-group col-md-6">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">  
+  <hr style="height: 2px; border: 0; color: #666; background-color: #008c00; margin-top: 0px !important;  margin-bottom: 0px !important; ">
+</div> 
+<div class="container" style="margin-top:15%;">
+	
+	
+	<div class="container-fluid">
+		<div class="input-group col-lg-12 col-md-12 col-xs-12">
+			<div class="form-group has-feedback col-md-6  col-xs-12" >
 				<label for="titulo" >Titulo:</label>
 				<input type="text" class="form-control" id="titulo" value="<?php echo $title ?>">
 			</div>
 		</div>
-		<div class="col-md-12">
-			<img width="30%" class="img-responsive" src="..<?php echo $image ?>"/>
+		
+		<div class="form-group has-feedback col-md-6  col-xs-12"  >
+			<label>Titulo de la Imagen</label>
+			<img width="70%" id="img_titulo" class="img-responsive" src="..<?php echo $image ?>"/><small><strong>NOTA: La imagen deberá tener como ancho tres veces la altura</strong></small>
 			<div class="input-file-wrapper">
 				<form enctype="multipart/form-data" class="formulario" >
         			<input type="file"  maxlength="1" accept="image/*" id="img_tema" />
 				</form>
+				
 			</div>
+			
 		</div>
 		<div class="col-md-12">
+			<div class="alert alert-danger" role="alert" style="display: none"  id="imagen">
+				<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true" ></span>
+  					Imagen no válida.
+		    </div>
+		</div>
+		
+		
+		<div class="col-md-12" >
 			<label for="usr">Descripción:</label>
 			<div id="summernote">
 				<?php echo $description ?>
@@ -33,12 +47,8 @@
 			<label style="display: none" id="id_tema"><?php echo $id_monthly_theme ?> </label>
 			<label style="display: none" id="mes"><?php echo $mounth ?> </label>
 		</div>
-		
-		<div id="contenedor">
-		  
-		</div>
-		
-		<button onclick="get_text()" type="button" class="btn btn-success">Actualizar</button>
-		
-	</div>  
+		<div class="form-group col-md-6  col-xs-12">
+			<button onclick="get_text()" type="button" class="btn btn-success">Actualizar</button>
+	    </div>
+	</div>
 </div>
--- statics/js/Login_admin/editor_summernote.js
+++ statics/js/Login_admin/editor_summernote.js
@@ -1,4 +1,58 @@
+function gcd(a, b){
+	return (b == 0) ? a : gcd(b, a%b);
+	
+}
+function readURL (input) {
+	if(input.files && input.files[0]){
+		var reader = new FileReader();
+		
+		reader.onload = function (e) {
+			$('#img_titulo').attr('src', e.target.result);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+  
+}
 
+
+function dimensiones () {
+  	var img_tema = document.getElementById('img_tema');
+  	img_tema.addEventListener("change", function() {
+	
+	var file = this.files[0];
+	var img = new Image();
+	var imagen = this;  
+	img.onload = function(){
+		var sizes = {
+			width: this.width,
+	  		height: this.height
+		};
+		URL.revokeObjectURL(this.src);
+		var w = sizes.width;
+		var h = sizes.height;
+		var r = gcd(w, h);
+		var ancho = w/r;
+		var altura = h/r;
+		
+		var triple = 3 * h;
+		
+		
+		if (w >= triple	) {
+			readURL(imagen);
+			return true;
+		} 
+		document.getElementById("imagen").style.display= 'inline';
+		return false;
+	}
+
+	var objectURL = URL.createObjectURL(file);
+	
+	console.log('change file', file);
+	console.log('change objectURL', objectURL);
+	img.src = objectURL;
+	});
+
+}
 							
 $(document).ready(function() {
 	$('#summernote').summernote({
@@ -10,17 +64,16 @@ function get_text() {
 	var id_mes = document.getElementById('mes').textContent;
 	var id_tema = document.getElementById('id_tema').textContent;
 	var titulo = document.getElementById('titulo').value;
-	//alert(id_mes);
+	
 	var markupStr = $('#summernote').summernote('code');
-
-	// var id = 1;
-	// var mes = 7;
-	// var titulo = 'Los espermatozoides hacen cálculos matemáticos muy complejos';
+	var img = document.getElementById('img_tema');
+	var img = new FormData();
 	var datos = {
 		id_monthly_theme : id_tema,
 		mounth : id_mes,
 		title : titulo,
 		description : markupStr,
+		image: img
 	};
 
 	var url_update = base_url + '/Home_admin_c/update_tema';
@@ -40,3 +93,4 @@ function get_text() {
 		}
 	});
 }
+window.onload = dimensiones;
