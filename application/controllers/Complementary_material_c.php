7e8b4122440beebc401fc255bd7ae06d13f1ed9f
LcHer <ceciferch@gmail.com>
2017-08-27 17:36:12 -0500

    CONTROLADOR HOME_ADMIN_C COMPLETO CONTROLADOR cOMPLEMETARY_MATERIAL_C EN PROCESO

--- application/controllers/Complementary_material_c.php
+++ application/controllers/Complementary_material_c.php
@@ -30,6 +30,46 @@ class Complementary_material_c  extends CI_Controller
 	    }
     }
 	
+	
+	function cargar_administrador(){
+		$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){
+	        $datos['bibliografia'] = $this ->bibliografia();
+		    $datos['ligas_interes'] = $this ->links();
+		    
+		    $datos["user_log"][0] = $this->session->userdata('user');
+		    $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+	        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+	        $datos['opt_menu_active']='opt_materiales';
+
+		    $this->load->view('header/head_v');
+		    $this->load->view('header/Menu_user_v', $datos);
+			$this->load->view('menu_usuario/Material_complementario_v',$datos);
+	    	$this->load->view('footer/footer_v');
+	    }else{
+	    	redirect(base_url());
+	    }
+	}
+	
+	
+	 function temas_biblio (){
+		$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){
+		    $datos["user_log"][0] = $this->session->userdata('user');
+		    $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+	        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+	       // $datos['opt_menu_active']='opt_materiales';
+			$biblio = $this->Complementary_material_m->lista_bibliografia(1);
+		    $this->load->view('header/head_v');
+		    $this->load->view('header/Menu_user_v', $datos);
+			$this->load->view('administrador/Editor_bibliografia_v',$biblio);
+	    	$this->load->view('footer/footer_v');
+	    }else{
+	    	redirect(base_url());
+	    }
+		
+	}
+	
 	/**
 	 * Funcion para obtener las bibliografias almacenadas en BD
 	 * @author Cecilia Hernandez Vasquez
@@ -38,11 +78,17 @@ class Complementary_material_c  extends CI_Controller
 	 * @version 1.0
 	 */	
 	
-	public function bibliografia(){
-	//pARA EDITAR VIA GET.	PARA MANDAR A LLAMAR VISTA  RETURN ECHO
-			$biblio = $this->Complementary_material_m->lista_bibliografia();
-			return $biblio;
-		}
+	public function editor_biblio(){
+		$id_biblio = $_GET["id_bibliography"]; 
+		$biblio = $this->Complementary_material_m->lista_bibliografia($id_biblio);
+			
+			$biblio = $this->Complementary_material_m->lista_bibliografia(1);
+			if ($biblio) {
+				echo "Actualizacion exitosa";
+			} else {
+				echo" Intentar mas tarde";
+			}
+	}
 	
 	/**
 	 * Funcion para obtener los links de los temas 
@@ -50,16 +96,19 @@ class Complementary_material_c  extends CI_Controller
 	 * @return 	NA
 	 * @param NA
 	 * @version 1.0
-	 */	
-	
-	
-	public function links(){
+	 */		
+	 function links(){
 		//pARA EDITAR VIA GET.	PARA MANDAR A LLAMAR VISTA RETURN ECHO
 			$links = $this->Complementary_material_m->lista_links();
 			return $links; 
 			
 		}
 	
+	FUNCTION bibliografia(){
+		$biblio = $this->Complementary_material_m->lista_bibliografia();
+		return $biblio;
+		
+	}
 	//METODOS PARA ACTUALIZAR LINK Y BIBLIOGRAFIA(CUENTA CON IMAGEN) 
 	
 	//BORRADO LOGICO (ELIMINAR) RETURN TRUE OR FALSE ECHO
--- application/controllers/Home_admin_c.php
+++ application/controllers/Home_admin_c.php
@@ -70,59 +70,77 @@ class Home_admin_c extends CI_Controller {
 		
 	}
 	
-	//guardar_calendario
+	/**
+	 * 
+	 * @author Cecilia Hernandez Vasquez
+	 * @return mensaje de Error u exito dependiendo de lo que pase.
+	 * @param NA
+	 * @version 1.0
+	 */
 	function guardar_calendario(){
-	$ruta = "statics/files/calendario_uam";
-	$mensaje = "";
-	
-	
-	//Validar primero que  se suba el numero correcto de archivos mandar error  en caso de que sea mayor a 3 o menor a 3
-	$total =3;
-	$cont=count($_FILES);
-		
-	if(count($_FILES)< 3){
-		echo "Se requiere un total de 3 archivos,";
-	}
-	
-	if(count($_FILES )>3){
-		echo "EL nùmero de archivos es mayor a 3";
-	}
-	if (count($_FILES )== 3){
-		
-	
-	
-	//echo "<pre>";
-	//print_r($_FILES);
-	//print_r("Archivos:"+count($_FILES ));
-	//echo "</pre>";
-	$i=0;
-	foreach ($_FILES as $key) {
-		if ($key['error'] == UPLOAD_ERR_OK) {
-			$type = explode( '/', $key['type'] );
-			$Nombre_Original = $key['name'];
-			$Temporal = $key['tmp_name'];
-			$destino = $ruta.$Nombre_Original;
-			
-			if(is_uploaded_file($_FILES['archivo'.$i]['tmp_name'])){
-				//echo "true";
-				copy($_FILES['archivo'.$i]['tmp_name'], $ruta.".".$type[1]); 
-				$i++;
-				echo "Se subio el archivo";
+		$ruta = "statics/files/calendario_uam";
+		$mensaje = "";
+		$total =3;
+		$cont=count($_FILES);
+
+		if(count($_FILES)< 3){
+			$mensaje= "Se requiere un total de 3 archivos";
+		}
+
+		if(count($_FILES )>3){
+			$mensaje= "EL nùmero de archivos es mayor a 3";
+		}
+		if (count($_FILES )== 3){
+			$i=0;
+			foreach ($_FILES as $key) {
+				if ($key['error'] == UPLOAD_ERR_OK) {
+					$type = explode( '/', $key['type'] );
+					if(is_uploaded_file($_FILES['archivo'.$i]['tmp_name'])){
+						copy($_FILES['archivo'.$i]['tmp_name'], $ruta.".".$type[1]); 
+						$i++;
+						$mensaje= "Se subio el archivo";
+					}
+				}
+				if ($key['error']!= 0) {
+					switch ($key['error']) {
+					case UPLOAD_ERR_INI_SIZE:
+						$mensaje = "El archivo subido supera la directiva upload_max_filesize en php.ini";  
+						break;
+
+					case UPLOAD_ERR_FORM_SIZE:
+						$mensaje="El archivo subido supera la directiva MAX_FILE_SIZE que se especificó en el formulario HTM";
+						break;
+
+					case UPLOAD_ERR_PARTIAL:
+						$mensaje="El archivo subido sólo se cargó parcialmente";
+						break;
+
+					case UPLOAD_ERR_NO_FILE:
+						$mensaje="Ningun archivo fue subido";
+					break;
+
+					case UPLOAD_ERR_NO_TMP_DIR:
+						$mensaje="Falta una carpeta temporal";
+					break;							
+
+					case UPLOAD_ERR_CANT_WRITE:
+						$mensaje="Error al escribir el archivo en el disco";
+					break;
+
+					case UPLOAD_ERR_EXTENSION:
+						$mensaje="Archivo de carga detenido por extensión";
+					break;
+
+					default: 
+						$mensaje = "Error desconocido en la carga"; 
+						break; 
+					}
+
+				}
 			}
-			//$type[1];
-			
-			//move_uploaded_file($Temporal, $destino);
 		}
-		/*if ($key['error'] == '') { 
-			$mensaje = "archivo subido";
-		}*/
-		if ($key['error'] != '') { 
-			$mensaje = "error".$key['error'];
-		}
+		echo $mensaje;
 	}
-	echo $mensaje;
-	}
-}
 
 	
 	/**
@@ -134,11 +152,28 @@ class Home_admin_c extends CI_Controller {
 	 */
 	
 	public function editor_tema(){
-		$id_tema =  $_GET['id_tema'];
-		$temas = $this->Home_admin_m->tema_mes($id_tema);
-		$this->load->view('header/head_v');
-		$this->load->view('administrador/Editor_tema_mes_v',$temas);
-		$this->load->view('footer/footer_v');
+		$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){     
+        	$type_user = $this->session->userdata('type_user');
+        	if($type_user == 1 || $type_user == 2){
+        		$datos['temas'] = $this->tema_mes();
+				
+				$datos["user_log"][0] = $this->session->userdata('user');
+		        $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+		        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+				$id_tema =  $_GET['id_tema'];
+				$temas = $this->Home_admin_m->tema_mes($id_tema);
+
+				$this->load->view('header/head_v');
+				$this->load->view('header/Menu_user_v', $datos);
+				$this->load->view('administrador/Editor_tema_mes_v',$temas);
+				$this->load->view('footer/footer_v');
+        	}else{
+        		redirect(base_url());
+        	}
+		}else{
+	    	redirect(base_url());
+	    }
 	}
 	
 	/**
@@ -150,14 +185,85 @@ class Home_admin_c extends CI_Controller {
 	 */
 	
 	public function update_tema(){
-		
-		$editor = $this->input->post('editor');
-        $update_editor=$this->Home_admin_m->actualizar_tema($editor);
-		if ($update_editor) {
-			echo "Actualizacion exitosa";
+		if ($_FILES!=NULL) {
+			$ruta = "statics/img/calendario_uam/";
+			$mensaje = "";
+			echo "<pre>";
+			print_r($_FILES);
+			echo "</pre>";
+			if ($_FILES['imagen']['error'] == UPLOAD_ERR_OK) {
+				//$Nombre_Original = $_FILES['name'];
+				$Temporal = $_FILES['imagen']['tmp_name'];
+				$image = 'statics/img/tema_mes/'.$_FILES['imagen']['name'];
+
+				move_uploaded_file($Temporal, $image);
+				$id_tema = $this->input->post('id_monthly_theme');
+				$id_mes = $this->input->post('id_monthly_theme');
+				$titulo = $this->input->post('title');
+				$descripcion = $this->input->post('description');
+				
+				
+				$datos = array( 'id_monthly_theme' => $id_tema,
+								'mounth' => $id_mes,
+								'title' => $titulo,
+								'description' => $descripcion,
+								'image' => '/'.$image, );
+				
+				$update_editor=$this->Home_admin_m->actualizar_tema($datos);
+				if ($update_editor) {
+					echo "Actualizacion exitosa";
+				} else {
+					echo" Intentar mas tarde";
+				}
+			}
+				if ($_FILES['imagen']['error']!= 0) {
+					switch ($_FILES['imagen']['error']) {
+					case UPLOAD_ERR_INI_SIZE:
+						echo  "El archivo subido supera la directiva upload_max_filesize en php.ini";  
+						break;
+
+					case UPLOAD_ERR_FORM_SIZE:
+						echo"El archivo subido supera la directiva MAX_FILE_SIZE que se especificó en el formulario HTM";
+						break;
+
+					case UPLOAD_ERR_PARTIAL:
+						echo"El archivo subido sólo se cargó parcialmente";
+						break;
+
+					case UPLOAD_ERR_NO_FILE:
+						echo"Ningun archivo fue subido";
+					break;
+
+					case UPLOAD_ERR_NO_TMP_DIR:
+						echo"Falta una carpeta temporal";
+					break;							
+
+					case UPLOAD_ERR_CANT_WRITE:
+						echo"Error al escribir el archivo en el disco";
+					break;
+
+					case UPLOAD_ERR_EXTENSION:
+						echo"Archivo de carga detenido por extensión";
+					break;
+
+					default: 
+						echo "Error desconocido en la carga"; 
+						break; 
+					}
+
+				}
+			//
+			
 		} else {
-			echo" Intentar mas tarde";
+			$editor = $this->input->post('editor');
+			$update_editor=$this->Home_admin_m->actualizar_tema($editor);
+			if ($update_editor) {
+				echo "Actualizacion exitosa";
+			} else {
+				echo" Intentar mas tarde";
+			}
 		}
+		
 	}
 	
 }
