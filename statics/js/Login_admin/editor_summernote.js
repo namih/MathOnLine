65f2ee8fcdd284442f86b4ed2717bba78427de37
NightlightMX <nightlightmx@gmail.com>
2017-08-26 19:12:55 -0500

    Modificaciones para editar tema de mes con  y sin imagen... validaciones de no seleccion de archivo de calendario

--- statics/js/Login_admin/editor_summernote.js
+++ statics/js/Login_admin/editor_summernote.js
@@ -2,58 +2,54 @@ function gcd(a, b){
 	return (b == 0) ? a : gcd(b, a%b);
 	
 }
+
 function readURL (input) {
 	if(input.files && input.files[0]){
 		var reader = new FileReader();
-		
 		reader.onload = function (e) {
 			$('#img_titulo').attr('src', e.target.result);
-		}
+		};
 		reader.readAsDataURL(input.files[0]);
 	}
-  
 }
-
 
 function dimensiones () {
-  	var img_tema = document.getElementById('img_tema');
-  	img_tema.addEventListener("change", function() {
-	
-	var file = this.files[0];
-	var img = new Image();
-	var imagen = this;  
-	img.onload = function(){
-		var sizes = {
-			width: this.width,
-	  		height: this.height
+	var img_tema = document.getElementById('img_tema');
+	img_tema.addEventListener("change", function() {
+		
+		var file = this.files[0];
+		var img = new Image();
+		var imagen = this;
+		img.onload = function(){
+			var sizes = {
+				width: this.width,
+				height: this.height
+			};
+			URL.revokeObjectURL(this.src);
+			var w = sizes.width;
+			var h = sizes.height;
+			var r = gcd(w, h);
+			var ancho = w/r;
+			var altura = h/r;
+			
+			var triple = 3 * h;
+			
+			if (w >= triple	) {
+				readURL(imagen);
+				return true;
+			}
+			document.getElementById("imagen").style.display= 'inline';
+			return false;
 		};
-		URL.revokeObjectURL(this.src);
-		var w = sizes.width;
-		var h = sizes.height;
-		var r = gcd(w, h);
-		var ancho = w/r;
-		var altura = h/r;
 		
-		var triple = 3 * h;
+		var objectURL = URL.createObjectURL(file);
 		
-		
-		if (w >= triple	) {
-			readURL(imagen);
-			return true;
-		} 
-		document.getElementById("imagen").style.display= 'inline';
-		return false;
-	}
-
-	var objectURL = URL.createObjectURL(file);
-	
-	console.log('change file', file);
-	console.log('change objectURL', objectURL);
-	img.src = objectURL;
+		console.log('change file', file);
+		console.log('change objectURL', objectURL);
+		img.src = objectURL;
 	});
-
 }
-							
+
 $(document).ready(function() {
 	$('#summernote').summernote({
 		lang : 'es-ES' // default: 'en-US'
@@ -66,31 +62,62 @@ function get_text() {
 	var titulo = document.getElementById('titulo').value;
 	
 	var markupStr = $('#summernote').summernote('code');
-	var img = document.getElementById('img_tema');
-	var img = new FormData();
-	var datos = {
-		id_monthly_theme : id_tema,
-		mounth : id_mes,
-		title : titulo,
-		description : markupStr,
-		image: img
-	};
-
-	var url_update = base_url + '/Home_admin_c/update_tema';
-	$.ajax({
-		type : 'post',
-		url : url_update,
-		data : {
-			editor : datos
-		},
-		datatype : 'json',
-		cache : false,
-		success : function() {
-			alert('OK');
-		},
-		error : function() {
-			alert('failure');
-		}
-	});
+		
+	if( document.getElementById("img_tema").files.length == 0 ){
+		var datos = {
+			id_monthly_theme : id_tema,
+			mounth : id_mes,
+			title : titulo,
+			description : markupStr,
+		};
+		
+		var url_update = base_url + '/Home_admin_c/update_tema';
+		$.ajax({
+			type : 'post',
+			url : url_update,
+			data : {editor:datos},
+			datatype : 'json',
+			cache : false,
+			success : function(msj) {
+				alert(msj);
+			},
+			error : function(msj) {
+				alert(msj);
+			}
+		});
+	} else {
+		alert(document.getElementById("img_tema").files.length);
+		var img = document.getElementById("img_tema");
+			
+		var formData = new FormData();
+		
+		formData.append('imagen', img.files[0]);
+		formData.append('id_monthly_theme', id_tema);
+		formData.append('mounth', id_mes);
+		formData.append('title', titulo);
+		formData.append('description', markupStr);
+		
+		
+		var url_update = base_url + '/Home_admin_c/update_tema';
+		$.ajax({
+			type : 'post',
+			url : url_update,
+			data : formData,
+			datatype : 'json',
+			async:false,
+			cache: false,
+			contentType: false,
+			processData: false,
+			success : function(msj) {
+				alert(msj);
+			},
+			error : function(msj) {
+				alert(msj);
+			}
+		});
+		
+	}
+	
 }
+
 window.onload = dimensiones;
--- statics/js/Login_admin/extensiones.js
+++ statics/js/Login_admin/extensiones.js
@@ -1,23 +1,28 @@
-	
-	
 function cargar_archivos () {
 	var fileInput = document.getElementById("archivos");
 	var filePath = fileInput.value;
+
+	var archivos = document.getElementById("archivos");
+	var archivo = archivos.files;
+	var archivos = new FormData();
+	
 	var allowedExtensions = /(.jpg|.jpeg|.png|.gif|.pdf|.docx|.doc)$/i;
-	if (!allowedExtensions.exec(filePath)) {
+	
+	if (archivo.length == 0) {
+		alert('No ha seleccionado ningun archivo');
+	} else if(!allowedExtensions.exec(filePath)){
 		alert('archivo no permitido');
-	}else{
+	} else {
 		var archivos = document.getElementById("archivos");
 		var archivo = archivos.files;
 	
 		var archivos = new FormData();
-	
-	
+		
 		for (var i=0; i < archivo.length; i++) {
 	  		archivos.append('archivo'+i, archivo[i]);
 		};	
 		$.ajax({
-			url: base_url+'/test_models_c/guardar',
+			url: base_url+'Home_admin_c/guardar_calendario',
 			type: 'POST',
 			data: archivos,
 			async:false,
@@ -27,7 +32,6 @@ function cargar_archivos () {
 			success: function(data){
 				alert('Archivo guardado '+data);
 				return data;
-				
 				},
 				error: function(){
 					console.log("Error to upload the image");
@@ -35,5 +39,4 @@ function cargar_archivos () {
 			});	
 		
 	};
-  	
 }
