1a9f2a1b194eade2e77799dd1bd4c58216b75cb1
LcHer <ceciferch@gmail.com>
2017-08-30 22:14:39 -0500

    Controlador para mostrar vista de editor links y biblio.

--- application/controllers/Complementary_material_c.php
+++ application/controllers/Complementary_material_c.php
@@ -44,15 +44,22 @@ class Complementary_material_c  extends CI_Controller
 
 		    $this->load->view('header/head_v');
 		    $this->load->view('header/Menu_user_v', $datos);
-			$this->load->view('menu_usuario/Material_complementario_v',$datos);
+			$this->load->view('administrador/Admin_material_v',$datos);
 	    	$this->load->view('footer/footer_v');
 	    }else{
 	    	redirect(base_url());
 	    }
 	}
+		
 	
-	
-	 function temas_biblio (){
+	/**
+	 * Funcion para obtener mostrar la vista de editor de la bibliografia
+	 * @author Cecilia Hernandez Vasquez
+	 * @return 	NA
+	 * @param NA
+	 * @version 1.0
+	 */	
+	 function editor_biblio (){
 		$login = $this->session->userdata('logged_in');
         if($login != null && $login == true){
 		    $datos["user_log"][0] = $this->session->userdata('user');
@@ -70,25 +77,114 @@ class Complementary_material_c  extends CI_Controller
 		
 	}
 	
+	
+	
 	/**
-	 * Funcion para obtener las bibliografias almacenadas en BD
+	 * Funcion para obtener mostrar la vista de editor de la bibliografia
 	 * @author Cecilia Hernandez Vasquez
 	 * @return 	NA
 	 * @param NA
 	 * @version 1.0
 	 */	
-	
-	public function editor_biblio(){
-		$id_biblio = $_GET["id_bibliography"]; 
-		$biblio = $this->Complementary_material_m->lista_bibliografia($id_biblio);
-			
-			$biblio = $this->Complementary_material_m->lista_bibliografia(1);
-			if ($biblio) {
-				echo "Actualizacion exitosa";
-			} else {
-				echo" Intentar mas tarde";
-			}
+	 function editor_links (){
+		$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){
+		    $datos["user_log"][0] = $this->session->userdata('user');
+		    $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+	        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+	       // $datos['opt_menu_active']='opt_materiales';
+			$links = $this->Complementary_material_m->lista_links(1);
+		    $this->load->view('header/head_v');
+		    $this->load->view('header/Menu_user_v', $datos);
+			$this->load->view('administrador/Editor_liga_v',$links);
+	    	$this->load->view('footer/footer_v');
+	    }else{
+	    	redirect(base_url());
+	    }
+		
 	}
+	
+	
+	/**
+	 * Funcion para obtener mostrar la vista de editor de la bibliografia
+	 * @author Cecilia Hernandez Vasquez
+	 * @return 	NA
+	 * @param NA
+	 * @version 1.0
+	 */	
+	 function agregar_biblio (){
+		$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){
+		    $datos["user_log"][0] = $this->session->userdata('user');
+		    $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+	        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+	       // $datos['opt_menu_active']='opt_materiales';
+			$biblio = $this->Complementary_material_m->lista_bibliografia(1);
+		    $this->load->view('header/head_v');
+		    $this->load->view('header/Menu_user_v', $datos);
+			$this->load->view('administrador/Editor_bibliografia_v');
+	    	$this->load->view('footer/footer_v');
+	    }else{
+	    	redirect(base_url());
+	    }
+		
+	}
+	
+	/**
+	 * Funcion para obtener mostrar la vista de editor de la bibliografia
+	 * @author Cecilia Hernandez Vasquez
+	 * @return 	NA
+	 * @param NA
+	 * @version 1.0
+	 */	
+	 function agregar_links (){
+		$login = $this->session->userdata('logged_in');
+        if($login != null && $login == true){
+		    $datos["user_log"][0] = $this->session->userdata('user');
+		    $menu = $this->etiquetas->menu_user($datos["user_log"][0]['id_user']);
+	        $datos['menu_user'] = $menu[$datos["user_log"][0]['type_user']];
+	       // $datos['opt_menu_active']='opt_materiales';
+			$links = $this->Complementary_material_m->lista_links(1);
+		    $this->load->view('header/head_v');
+		    $this->load->view('header/Menu_user_v', $datos);
+			$this->load->view('administrador/Editor_liga_v',$links);
+	    	$this->load->view('footer/footer_v');
+	    }else{
+	    	redirect(base_url());
+	    }
+		
+	}
+	
+	/**
+	 * Funcion para actualizar  las bibliografias almacenadas en BD
+	 * @author Cecilia Hernandez Vasquez
+	 * @return mensaje de error o de exito en caso de que se haya realizado la actualizacion.
+	 * @param NA
+	 * @version 1.0
+	 */	
+	
+	public function actualizar_biblio(){
+		// con imagen
+	}
+	
+	/**
+	 * Funcion para actualizar  las bibliografias almacenadas en BD
+	 * @author Cecilia Hernandez Vasquez
+	 * @return mensaje de error o de exito en caso de que se haya realizado la actualizacion.
+	 * @param NA
+	 * @version 1.0
+	 */	
+	
+	public function actualizar_link(){
+		$id_link = $this->input->post('link');
+		$link = $this->Complementary_material_m->actualizar_link($id_link);
+		if ($id_link) {
+			echo "Actualizacion exitosa";
+		} else {
+			echo" Intentar mas tarde";
+		}
+	}
+	
 	
 	/**
 	 * Funcion para obtener los links de los temas 
@@ -98,7 +194,6 @@ class Complementary_material_c  extends CI_Controller
 	 * @version 1.0
 	 */		
 	 function links(){
-		//pARA EDITAR VIA GET.	PARA MANDAR A LLAMAR VISTA RETURN ECHO
 			$links = $this->Complementary_material_m->lista_links();
 			return $links; 
 			
