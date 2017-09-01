c85bd8842a43837de4ad1e5a61b7250e2a13ab21
krmncc <carmen.chavez.conde@gmail.com>
2017-09-01 16:27:21 -0500

    Correciones

--- application/views/administrador/Admin_material_v.php
+++ application/views/administrador/Admin_material_v.php
@@ -7,6 +7,7 @@
 				<table class="table ">
     				<thead>
       					<tr>
+      						<th></th>
         					<th>Titulo</th>
         					<th>Liga</th>
         					<th>Editar</th>
@@ -17,6 +18,7 @@
     				</tbody>
     					<?php for ($i=0;$i < count($ligas_interes);$i++){ ?>
     						<tr>
+    							<td> <span class="glyphicon glyphicon-remove" style="color:red"></span></td>
     							<td> <?php echo $ligas_interes[$i]['title'] ?></td>
     							<td> <?php echo $ligas_interes[$i]['link'] ?></td>
     							<td> <a href="<?php echo base_url(); ?>Complementary_material_c/editor_liga?id_links_interest=<?php echo $ligas_interes[$i]['id_links_interest'] ?>"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> </a> </td>
@@ -31,6 +33,7 @@
 				<table class="table ">
     				<thead>
       					<tr>
+      						<th></th>
         					<th>Titulo</th>
         					<th>Autor </th>
         					<th>Editar</th>
@@ -41,6 +44,7 @@
     				</tbody>
     					<?php for ($i=0;$i < count($bibliografia);$i++){ ?>
     						<tr>
+    							<td> <span class="glyphicon glyphicon-remove" style="color:red"></span></td>
     							<td> <?php echo $bibliografia[$i]['title'] ?></td>
     							<td> <?php echo $bibliografia[$i]['author'] ?></td>
     							<td> <a href="<?php echo base_url(); ?>Complementary_material_c/editor_biblio?id_bibliography=<?php echo $bibliografia[$i]['id_bibliography'] ?>"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> </a> </td>
@@ -53,3 +57,22 @@
 		</div>	
 	</div>
 	
+	<div id="Eliminar" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Eliminar</h4>
+      </div>
+      <div class="modal-body">
+        <p><strong> ¿Estás seguro que deseas eliminar el tema del mes?</strong></p>
+      </div>
+      <div class="modal-footer">
+      	<a href="<?php echo base_url(); ?>" class="btn btn-success" role="button">Eliminar</a>
+      </div>
+    </div>
+
+  </div>
+</div>
