96a498b2de7349bc7cad6ab3dbc1e39dd7ec850f
Cesar Padilla <sisbyte@Mac-mini-de-LDCBI.local>
2017-09-07 12:27:33 -0500

    Modificaciones menores al administrador de evaluaciones

---
+++ application/views/administrador/Editor_evaluacion_v.php
@@ -0,0 +1,25 @@
+<script src="http://www.wiris.net/demo/editor/editor"></script>
+
+<!-- include summernote css/js-->
+<script src="<?php echo base_url(); ?>statics/js/summernote/summernote.js"></script>
+<link rel="stylesheet" href="<?php echo base_url(); ?>statics/js/summernote/summernote.css">
+
+<!-- Lenguaje español -->
+<script src="<?php echo base_url(); ?>statics/js/summernote/lang/summernote-es-ES.js"></script>
+<script src="<?php echo base_url(); ?>statics/js/Login_admin/editor_summernote.js"></script>
+
+<script src="<?php echo base_url(); ?>statics/js/editor_evaluacion.js"></script>
+<div class="container-fluid">
+	<div class="input-group col-lg-12 col-md-12 col-xs-12">
+		<h3>Editor de ecuaciones</h3>
+		<div id="editorContainer">
+			
+		</div>
+	</div>
+	<div class="input-group col-lg-12 col-md-12 col-xs-12">
+		<label>Pregunta</label>
+		<div id="summernote">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</div>	
+	</div>
+</div>
