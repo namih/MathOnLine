7419eca3dc69112bd4b74777e35e4e606a5548c3
krmncc <carmen.chavez.conde@gmail.com>
2017-09-08 18:38:28 -0500

    array de checkbox

--- statics/js/Login_admin/material_complementario.js
+++ statics/js/Login_admin/material_complementario.js
@@ -44,68 +44,74 @@ function get_bibliografia () {
 	var anio = document.getElementById('anio').value;
 	var pais = document.getElementById('pais').value;
 	var tema = document.getElementById('tema').value;
+	
+	if (id_biblio == null) {
+		//Aquí va código para agregar nueva bibliografia
+	} else{
+			
+		var url_update = base_url + '/Complementary_material_c/actualizar_biblio';
 
-	var url_update = base_url + '/Complementary_material_c/actualizar_biblio';
+		if (document.getElementById("img_libro").files.length == 0) {
+			var datos = {
+				id_user : id_usuario,
+				id_bibliography : id_biblio,
+				title : titulo,
+				author : autor,
+				editorial : editorial,
+				year : anio,
+				place_publication : pais,
+				id_theme : tema,
+			};
 
-	if (document.getElementById("img_libro").files.length == 0) {
-		var datos = {
-			id_user : id_usuario,
-			id_bibliography : id_biblio,
-			title : titulo,
-			author : autor,
-			editorial : editorial,
-			year : anio,
-			place_publication : pais,
-			id_theme : tema,
+			$.ajax({
+				type : 'post',
+				url : url_update,
+				data : {
+					editor : datos
+				},
+				datatype : 'json',
+				cache : false,
+				success : function(msj) {
+					alert(msj);
+				},
+				error : function(msj) {
+					alert(msj);
+				}
+			});
+
+		} else {
+
+			var img_libro = document.getElementById('img_libro');
+
+			var formData = new FormData();
+
+			formData.append('imagen', img_libro.files[0]);
+			formData.append('id_user', id_usuario);
+			formData.append('id_bibliography', id_biblio);
+			formData.append('title', titulo);
+			formData.append('author', autor);
+			formData.append('editorial', editorial);
+			formData.append('year', anio);
+			formData.append('place_publication', pais);
+			formData.append('id_theme', tema);
+
+			$.ajax({
+				type : 'post',
+				url : url_update,
+				data : formData,
+				datatype : 'json',
+				async : false,
+				cache : false,
+				contentType : false,
+				processData : false,
+				success : function(msj) {
+					alert(msj);
+				},
+				error : function(msj) {
+					alert(msj);
+				}
+			});
 		};
-
-		$.ajax({
-			type : 'post',
-			url : url_update,
-			data : {
-				editor : datos
-			},
-			datatype : 'json',
-			cache : false,
-			success : function(msj) {
-				alert(msj);
-			},
-			error : function(msj) {
-				alert(msj);
-			}
-		});
-
-	} else {
-		var img_libro = document.getElementById('img_libro');
-
-		var formData = new FormData();
-
-		formData.append('imagen', img_libro.files[0]);
-		formData.append('id_user', id_usuario);
-		formData.append('id_bibliography', id_biblio);
-		formData.append('title', titulo);
-		formData.append('author', autor);
-		formData.append('editorial', editorial);
-		formData.append('year', anio);
-		formData.append('place_publication', pais);
-		formData.append('id_theme', tema);
-
-		$.ajax({
-			type : 'post',
-			url : url_update,
-			data : formData,
-			datatype : 'json',
-			async : false,
-			cache : false,
-			contentType : false,
-			processData : false,
-			success : function(msj) {
-				alert(msj);
-			},
-			error : function(msj) {
-				alert(msj);
-			}
-		});
 	};
 
 }
@@ -118,32 +124,40 @@ function get_link() {
 	var titulo = document.getElementById('titulo').value;
 	var descripcion = document.getElementById('descripcion').value;
 	var link = document.getElementById('liga').value;
-
-	var url_update = base_url + '/Complementary_material_c/actualizar_link';
-
-	var datos = {
-		id_user : id_usuario,
-		id_links_interest : id_liga,
-		title : titulo,
-		description : descripcion,
-		link : link
-	};
 	
-	$.ajax({
-		type : 'post',
-		url : url_update,
-		data : {
-			link : datos
-		},
-		datatype : 'json',
-		cache : false,
-		success : function(msj) {
-			alert(msj);
-		},
-		error : function(msj) {
-			alert(msj);
-		}
-	});
+	if (id_liga == null) {
+		//Aquí va código para agregar nueva liga
+	} else{
+		var url_update = base_url + '/Complementary_material_c/actualizar_link';
+
+	
+		var datos = {
+			id_user : id_usuario,
+			id_links_interest : id_liga,
+			title : titulo,
+			description : descripcion,
+			link : link
+		};
+
+		$.ajax({
+			type : 'post',
+			url : url_update,
+			data : {
+				link : datos
+			},
+			datatype : 'json',
+			cache : false,
+			success : function(msj) {
+				alert(msj);
+			},
+			error : function(msj) {
+				alert(msj);
+			}
+		}); 
+
+	};
+
+	
 
 }
 
@@ -157,14 +171,47 @@ function liga_seleccionada() {
 }
 
 function bibliografia_seleccionada() {
-	$(document).ready(function() {
-		$("#eliminar_l").on("click", function() {
-			var seleccion = $("#liga").is(":checked");
-
-		});
-	});
+	var num_biblio = document.getElementById('numero_biblio').textContent;
+	
+	var seleccionados = [];
+	
+	for (var i=1; i <= num_biblio; i++) {
+	  var seleccion = $("#biblio_"+i).is(":checked");
+	  if (seleccion == true) {
+	  	var x = $('#biblio_'+i).attr("name");
+	  	seleccionados.push(x);
+	  	
+	  };
+	};
+	alert(seleccionados.toString());
+	console.log(seleccionados);
+	var botones = '<button onclick="borrar_biblio(\'';
+	botones += seleccionados.toString();
+	botones +=	'\')" type="button" class="btn btn-warning" data-dismiss="modal">Eliminar</button>';
+ 	botones += '<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>';
+	document.getElementById('botones').innerHTML = botones;
+	$('#Eliminar').modal();
 }
 
-function add_link () {
-  
+function borrar_biblio (lista) {
+  	console.log(lista);
+  	var pos = lista.split(",");
+  	var url_update = base_url + '/Complementary_material_c/eliminar_biblio';
+  	
+  	$.ajax({
+			type : 'post',
+			url : url_update,
+			data : {
+				biblio : pos
+			},
+			datatype : 'json',
+			cache : false,
+			success : function(msj) {
+				alert(msj);
+			},
+			error : function(msj) {
+				alert(msj);
+			}
+		}); 
+  	
 }
