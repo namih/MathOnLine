3ab7efd25ecdbd6cf6ecdad1b82c46ca01d14967
Cesar Padilla <nightlight@Mac-mini-de-LDCBI.local>
2017-09-29 16:27:41 -0500

    Template de controlador y modelo para los tutoriales

---
+++ application/models/Tutorial_m.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Tutorial_m extends CI_Model {
+			
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		
+		/**
+		 * Descripcion
+		 * 
+		 * @author Julio Cesar Padilla Dorantes
+		 * @return 
+		 * @param NA
+		 * @version 1.0
+		 */
+		 public function create_tutorial($tutorial)
+		 {
+		 	if ($tutorial != NULL) {
+				 
+			 } else {
+				 return NULL;
+			 }
+			 
+		 }
+		 
+		 /**
+		 * Descripcion
+		 * 
+		 * @author Julio Cesar Padilla Dorantes
+		 * @return 
+		 * @param NA
+		 * @version 1.0
+		 */
+		 public function update_progress($tutorial)
+		 {
+		 	if ($tutorial != NULL) {
+				 
+			 } else {
+				 return NULL;
+			 }
+			 
+		 }
+		 
+		 /**
+		 * Descripcion
+		 * 
+		 * @author Julio Cesar Padilla Dorantes
+		 * @return 
+		 * @param NA
+		 * @version 1.0
+		 */
+		 public function finish_tutorial($tutorial)
+		 {
+		 	if ($tutorial != NULL) {
+				 
+			 } else {
+				 return NULL;
+			 }
+			 
+		 }
+		 
+}
+
+/* End of file Home_student_m.php */
+/* Location: ./application/models/Home_student_m.php */
