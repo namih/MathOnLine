c73c63caf2ae0d3b30167fb8a5a3cfb4a759860a
NightlightMX <nightlightmx@gmail.com>
2017-10-21 16:09:02 -0500

    Inicio de tutoriales de comparacion de fracciones

---
+++ application/views/tutorial/numeros/fracc_pct_rzns_pro/comparacion/diapositiva_1_v.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+	<p align="justify">¿Recurdas el problema del inicio? Un grupo de estudiantes va de visita. A un museo y lleva baguettes para repartir a la hora del almuerzo. ¿Qué equipo tiene la mayor cantidad de baguettes para comer por persona?</p>
+	<br />
+	<div id="imagen">
+		<img class="img-responsive" src="<?php echo base_url(); ?>statics/img/otros/empty-image.png	" />
+	</div>
+	<br />
+	<p align="justify">Para responder, tuviste que comparar las 4 fracciones y decidir qué porción de baguette es mayor. Como contabas con las representaciones de las baguettes, pudiste comparar el tamaño de las barras. ¿Cómo comparas las fracciones sin las barras?</p>
+</div>
