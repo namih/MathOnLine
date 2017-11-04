8c38632fe6d58adb6efb9e68627166a6548a08fa
Forgum77 <alf861502@gmail.com>
2017-11-04 12:14:37 -0600

    diapositiva 61 lineal 1 variable

---
+++ application/views/tutorial/algebra/op_alg/exp_alg_lin_una_var/diapositiva_61_v.php
@@ -0,0 +1,9 @@
+<script src="<?php echo base_url(); ?>statics/js/tutorial/algebra/op_alg/exp_alg_lin_una_var/variables.js"></script>
+<div class="container-fluid">
+	<div>
+		<p>En una expresión algebraica, los coeficientes también pueden ser negativos.</p>
+		<p>Para simplificar la expresión, suma los coeficientes.</p>
+		<p>3x +(-7)x = -4x</p>
+		<p>3 + (-7) = -4</p>
+	</div>
+  </div>
