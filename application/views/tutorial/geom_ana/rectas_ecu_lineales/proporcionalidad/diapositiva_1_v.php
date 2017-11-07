542e42e17e0f6d7d64464179665d9f920bb781f3
mimis <fatal.experience@gmail.com>
2017-11-07 12:26:07 -0600

    prueba tutorial

---
+++ application/views/tutorial/geom_ana/rectas_ecu_lineales/proporcionalidad/diapositiva_1_v.php
@@ -0,0 +1,75 @@
+<div class="container-fluid">
+	<style type="text/css">
+		.pregunta{
+			color: #2e75b6;
+			text-align: justify;
+			font-weight: bold;
+		}
+		.instruccion{
+			color: #000000;
+			text-align: justify;
+			font-weight: normal;
+		}
+	</style>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
+		<div class="instruccion">
+			<p>	En este tutorial aprenderás cómo distinguir si existe una relación de proporcionalidad directa entre dos conjuntos de números.
+			<br>
+			Lee con atención cada paso de las explicaciones y dedícale suficiente tiempo a los ejercicios hasta que domines el tema.<br></p>
+		</div>
+		<div class="pregunta"><p>1. La señora Rosales está a dieta. Va de compras al supermercado para comprar jamón de pavo y le dan 8 rebanadas que pesan 300 gramos en total.</p>
+		</div>
+		<div class="instruccion">
+		<p>Completa la siguiente tabla que relaciona el número de rebanadas con el peso total.</p>
+		</div>
+		<div align="left"><p><label>Ejemplos</label></p></div>		
+		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" align="center">
+			<table class="table table-responsive table-striped table-bordered table-condensed" style="max-width:0%; margin:0 auto;">
+	            <thead>
+	                <tr class="success">
+	                    
+	                    <th>Rebanadas</th>
+	                    <th>Gramos</th>
+	                </tr>
+	            </thead>
+	            <tbody>
+	            	<tr>
+	            		<td>
+	            			<input class="input-sm2" type="text" id="reb_1" />
+	            		</td>
+	            		<td>
+	            			<input class="input-sm2" type="text" id="gram_1"/>
+	            		</td>
+	            	</tr>
+	            	<tr>
+	            		<td>
+	            			<input class="input-sm2" type="text" id="reb_2" />
+	            		</td>
+	            		<td>
+	            			<input class="input-sm2" type="text" id="gram_2" />
+	            		</td>
+	            	</tr>
+	            	<tr>
+	            		<td>
+	            			<input class="input-sm2" type="text" id="reb_3" />
+	            		</td>
+	            		<td>
+	            			<input class="input-sm2" type="text"  id="gram_3"/>
+	            		</td>
+	            	</tr>
+	            	<tr>
+	            		<td>
+	            			<input class="input-sm2" type="text" id="reb_4" />
+	            		</td>
+	            		<td>
+	            			<input class="input-sm2" type="text"  id="gram_4"/>
+	            		</td>
+	            	</tr>
+	            </tbody>
+	        </table>
+		</div>
+		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" align="center" style="border: 1px solid;">
+			
+		</div>		
+	</div>
+  </div>
