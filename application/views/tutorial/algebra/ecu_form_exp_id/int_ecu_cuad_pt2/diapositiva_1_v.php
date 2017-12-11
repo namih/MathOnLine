a7216b9604fe7217dd6b4ec6f0c2c3943c6d67d9
Cesar Padilla <nightlightmx@Mac-mini-de-ldcbi.local>
2017-12-11 15:56:49 -0600

    Primer ggb applet

--- application/views/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2/diapositiva_1_v.php
+++ application/views/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2/diapositiva_1_v.php
@@ -1,11 +1,20 @@
 <script src="<?php echo base_url(); ?>statics/js/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2.js"></script>
+<script src="https://cdn.geogebra.org/apps/deployggb.js"></script>
+<script type="text/javascript" charset="utf-8">
+	var applet = new GGBApplet({filename: base_url+"statics/ggb/int_ecu_cuad_pt2_d1.ggb","showToolbar":false, height: 500, "showMenuBar": false}, true);
+	window.addEventListener("load", function() {
+        applet.inject('applet_container');	
+    });
+</script>
 
 <div class="container-fluid">
-	<div class="col-xs-12 col-sm-12 col-lg-8">
-	  <h1>Contenido texto</h1>
+	<div class="col-xs-12 col-sm-12 col-lg-12">
+	  <p align="justify">Las ecuaciones cuadráticas pueden asociarse con funciones del tipo <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><mi>a</mi><msup><mi>x</mi><mn>2</mn></msup><mo>+</mo><mi>b</mi><mi>x</mi><mo>+</mo><mi>c</mi></math>, cuya gráfica representa una parábola.</p>
+	  <p align="justify">Observa la gráfica de <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><msup><mi>x</mi><mn>2</mn></msup><mo>-</mo><mn>4</mn></math>.</p>
+	  <p align="justify">Cambia el valor de x con las flechas y observa la ubicación del punto P(x,y) sobre la gráfcia.</p>
+	  <p>¿Cuántas intersecciones tiene la gráfica con el eje x? <input type="text" name="some_name" value="" id="some_name"/></p>
 	</div>
-	<div class="col-xs-12 col-sm-12 col-lg-4">
-	  <h1>Gráfica</h1>
+	<div class="col-xs-12 col-sm-12 col-lg-12" id="applet_container">
 	</div>
 </div>
 <br />
