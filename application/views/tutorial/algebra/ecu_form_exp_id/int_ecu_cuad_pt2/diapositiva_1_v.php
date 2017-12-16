1a87e234b1f3938202d859bfef40c47f0e5bd02e
Cesar Padilla <nightlightmx@Mac-mini-de-ldcbi.local>
2017-12-16 15:03:57 -0600

    Mejora del applet

--- application/views/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2/diapositiva_1_v.php
+++ application/views/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2/diapositiva_1_v.php
@@ -3,18 +3,18 @@
 <script type="text/javascript" charset="utf-8">
 	var applet = new GGBApplet({filename: base_url+"statics/ggb/int_ecu_cuad_pt2_d1.ggb","showToolbar":false, height: 500, "showMenuBar": false}, true);
 	window.addEventListener("load", function() {
-        applet.inject('applet_container');	
+        applet.inject('applet_container');
     });
 </script>
 
 <div class="container-fluid">
 	<div class="col-xs-12 col-sm-12 col-lg-12">
-	  <p align="justify">Las ecuaciones cuadráticas pueden asociarse con funciones del tipo <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><mi>a</mi><msup><mi>x</mi><mn>2</mn></msup><mo>+</mo><mi>b</mi><mi>x</mi><mo>+</mo><mi>c</mi></math>, cuya gráfica representa una parábola.</p>
+	  <p align="justify">Las ecuaciones cuadráticas pueden asociarse con funciones del tipo <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><mi>a</mi><msup><mi>x</mi><mn>2</mn></msup><mo>+</mo><mi>b</mi><mi>x</mi><mo>+</mo><mi>c</mi></math>, cuya gráfica representa una parábola. Observa como cambian las coordenadas del punto rojo al moverlo.</p>
 	  <p align="justify">Observa la gráfica de <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><msup><mi>x</mi><mn>2</mn></msup><mo>-</mo><mn>4</mn></math>.</p>
-	  <p align="justify">Cambia el valor de x con las flechas y observa la ubicación del punto P(x,y) sobre la gráfica.</p>
+	  <p align="justify">Cambia el valor de x con las flechas y observa la ubicación del punto A(x,y) sobre la gráfica.</p>
 	  <div class="btn-group" id="opciones" class="col-xs-12 col-sm-12 col-lg-12">
 		<button onclick="disminuir_1()" type="button" class="btn btn-info"><</button>
-		<button id="valor" type="button" class="btn">0</button>
+		<button id="valor" type="button" class="btn">-</button>
 		<button onclick="incrementar_1()" type="button" class="btn btn-info">></button>
 	</div>
 	<p>¿Cuántas intersecciones tiene la gráfica con el eje x? <input placeholder="0" style="text-align: center" onchange="validar_1()" size="1" type="text" id="interseccion"></p>
--- statics/js/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2.js
+++ statics/js/tutorial/algebra/ecu_form_exp_id/int_ecu_cuad_pt2.js
@@ -1,3 +1,5 @@
+var x = -3;
+
 function correcto() {
 	$('#correcta').show();
 	$("#correcta").fadeTo(2000, 500).slideUp(500, function(){
@@ -14,9 +16,97 @@ function error() {
 
 function incrementar_1() {
 	
-	var x = 0;
+	var applet = document.ggbApplet;
+		
+	switch (x) {
+		case -3:
+			applet.evalCommand("res=ZoomOut[2]"); 
+			applet.evalCommand("A = Point[{-3, 5}]");
+			document.getElementById("valor").innerHTML = "-3";
+			x++;
+			break;
+		case -2:
+			applet.evalCommand("A = Point[{-2, 0}]");
+			document.getElementById("valor").innerHTML = "-2";
+			x++;
+			break;
+		case -1:
+			applet.evalCommand("A = Point[{-1, -3}]");
+			document.getElementById("valor").innerHTML = "-1";
+			x++;
+			break;
+		case 0:
+			applet.evalCommand("A = Point[{-0, -4}]");
+			document.getElementById("valor").innerHTML = "0";
+			x++;
+			break;
+		case 1:
+			applet.evalCommand("A = Point[{1, -3}]");
+			document.getElementById("valor").innerHTML = "1";
+			x++;
+			break;
+		case 2:
+			applet.evalCommand("A = Point[{2, 0}]");
+			document.getElementById("valor").innerHTML = "2";
+			x++;
+			break;
+		case 3:
+			applet.evalCommand("A = Point[{3, 5}]");
+			document.getElementById("valor").innerHTML = "3";
+			x++;
+			break;
+	}
+}
+
+function disminuir_1() {
 	
 	var applet = document.ggbApplet;
-	
-	applet.evalCommand("P = Point[{-5, 21}]"); 
+		
+	switch (x) {
+		case -2:
+			applet.evalCommand("res=ZoomIn[2]"); 
+			applet.evalCommand("Delete(A)");
+			document.getElementById("valor").innerHTML = "-";
+			x--;
+			break;
+		case -1:
+			applet.evalCommand("A = Point[{-3, 5}]");
+			document.getElementById("valor").innerHTML = "-3";
+			x--;
+			break;
+		case 0:
+			applet.evalCommand("A = Point[{-2, 0}]");
+			document.getElementById("valor").innerHTML = "-2";
+			x--;
+			break;
+		case 1:
+			applet.evalCommand("A = Point[{-1, -3}]");
+			document.getElementById("valor").innerHTML = "-1";
+			x--;
+			break;
+		case 2:
+			applet.evalCommand("A = Point[{-0, -4}]");
+			document.getElementById("valor").innerHTML = "0";
+			x--;
+			break;
+		case 3:
+			applet.evalCommand("A = Point[{1, -3}]");
+			document.getElementById("valor").innerHTML = "1";
+			x--;
+			break;
+		case 4:
+			applet.evalCommand("A = Point[{2, 0}]");
+			document.getElementById("valor").innerHTML = "2";
+			x--;
+			break;
+	}
+}
+
+function validar_1 () {
+	var interseccion = document.getElementById('interseccion').value;
+	if (interseccion == 2) {
+		correcto();
+	} else{
+		error();
+	};
 }
