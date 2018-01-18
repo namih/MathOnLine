1626116ae7eabc87911162ef13ae9c36590b4f06
Cesar Padilla <nightlightmx@MacBook-Pro-de-Cesar.local>
2018-01-18 08:21:23 -0600

    Nuevo csv y primer ejercicios adicionales

--- statics/js/tutorial/numeros/fracc_pct_rzns_pro/comparacion.js
+++ statics/js/tutorial/numeros/fracc_pct_rzns_pro/comparacion.js
@@ -27,3 +27,13 @@ function fraction_right () {
 	document.getElementById('equation_r').innerHTML = '<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mi>'+num_y+'</mi><mi>'+den_y+'</mi></mfrac><mo>&#xA0;</mo><mo>=</mo><mo>&#xA0;</mo><mfrac><mstyle displaystyle="true"><mn>2</mn><mo>&#xA0;</mo><mo>&#xD7;</mo><mo>&#xA0;</mo><mi>'+y+'</mi></mstyle><mstyle displaystyle="true"><mn>4</mn><mo>&#xA0;</mo><mo>&#xD7;</mo><mo>&#xA0;</mo><mi>'+y+'</mi></mstyle></mfrac></math>';
 	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
+
+function show_second_2 () {
+	document.getElementById('first').style.display = 'none';
+	document.getElementById('second').style.display = 'inline';
+}
+
+function show_third_2 () {
+	document.getElementById('second').style.display = 'none';
+	document.getElementById('third').style.display = 'inline';
+}
