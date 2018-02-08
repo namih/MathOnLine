c0a620ccded3dfa05b65974b4904b5e5da5780a0
Carmen Chavez <krmnmorrison@Mac-mini-de-ldcbi.local>
2018-02-08 15:49:48 -0600

    operacion: multiplicacion y division

--- statics/js/tutorial/numeros/fracc_pct_rzns_pro/op-mult_div.js
+++ statics/js/tutorial/numeros/fracc_pct_rzns_pro/op-mult_div.js
@@ -1,8 +1,77 @@
-function fraccion_1 () {
+function acierto () {
+  	$('#correcta').show();
+	$("#correcta").fadeTo(2000, 500).slideUp(500, function(){
+	$("#correcta").slideUp(500);
+	});	
+}
+
+function error () {
+  	$('#error').show();
+	$("#error").fadeTo(2000, 500).slideUp(500, function(){
+	$("#error").slideUp(500);
+	});
+}
+
+function fraccion_2 () {
   	var numerador = document.getElementById('numerador_a').value;
   	var denominador = document.getElementById('denominador_a').value;
   	if (numerador != '' && denominador != '') {
 		if (numerador == 10 && denominador == 3) {
+  			
+  			acierto();	
+  		}else{
+  			
+  			error();
+  		};
+  	};
+}
+
+function fraccion_3 () {
+  	var numerador = document.getElementById('numerador_a').value;
+  	var denominador = document.getElementById('denominador_a').value;
+  	if (numerador != '' && denominador != '') {
+		if (numerador == 10 && denominador == 3) {
+  			
+  			acierto();	
+  		}else{
+  			
+  			error();
+  		};
+  	};
+}
+
+function fraccion_4 () {
+  	var numerador = document.getElementById('numerador_a').value;
+  	var denominador = document.getElementById('denominador_a').value;
+  	if (numerador != '' && denominador != '') {
+		if (numerador == 10 && denominador == 3) {
+  			
+  			acierto();	
+  		}else{
+  			
+  			error();
+  		};
+  	};
+}
+
+function fraccion_mixta_5 () {
+	var entero_h = document.getElementById('entero').value;
+  	var numerador_h = document.getElementById('numerador').value;
+  	var denominador_h = document.getElementById('denominador').value;
+  	if (entero_h != '' && numerador_h != '' && denominador_h != '') {
+		if (entero_h == 3 && numerador_h == 1 && denominador_h == 3) {
+  			acierto();
+  		}else{
+  			error();
+  		};
+  	};
+}
+
+function fraccion_6 () {
+  	var numerador = document.getElementById('numerador_a').value;
+  	var denominador = document.getElementById('denominador_a').value;
+  	if (numerador != '' && denominador != '') {
+		if (numerador == 18 && denominador == 8) {
   			
   			acierto();	
   		}else{
