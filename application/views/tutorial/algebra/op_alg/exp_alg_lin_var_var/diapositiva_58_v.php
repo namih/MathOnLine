602bc505486a3eb3ef9ac037de05910a738525b6
Forgum77 <alf861502@gmail.com>
2018-04-07 14:24:28 -0500

    correcciones lineal varias variables

--- application/views/tutorial/algebra/op_alg/exp_alg_lin_var_var/diapositiva_58_v.php
+++ application/views/tutorial/algebra/op_alg/exp_alg_lin_var_var/diapositiva_58_v.php
@@ -1,7 +1,7 @@
 <script src="<?php echo base_url(); ?>statics/js/tutorial/algebra/op_alg/exp_alg_lin_var_var/var_variables.js"></script>
 <div class="container-fluid">
 	<div>
-		<p>Escribe el valor de la figuras juntas cuando <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>n</mi></math> y <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>m</mi></math> toman el valor que se indica:</p>
+		<p>Escribe el valor de las figuras juntas cuando <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>n</mi></math> y <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>m</mi></math> toman el valor que se indica:</p>
 		<p><math xmlns="http://www.w3.org/1998/Math/MathML"><mi>n</mi><mo>&#xA0;</mo><mo>=</mo><mo>&#xA0;</mo><mn>4</mn></math></p>
 		<p><math xmlns="http://www.w3.org/1998/Math/MathML"><mi>m</mi><mo>&#xA0;</mo><mo>=</mo><mo>&#xA0;</mo><mn>2</mn></math></p>
 		<p><math xmlns="http://www.w3.org/1998/Math/MathML"><mn>2</mn><mi>n</mi><mo>&#xA0;</mo><mo>+</mo><mo>&#xA0;</mo><mfrac><mi>n</mi><mn>2</mn></mfrac><mo>&#xA0;</mo><mo>+</mo><mo>&#xA0;</mo><mn>2</mn><mi>m</mi><mo>&#xA0;</mo><mo>=</mo></math> <input type="text" style="text-align: center" onblur="acierto(<?php echo $paginador['actual']; ?>)" id="1" size="4" /></p>
