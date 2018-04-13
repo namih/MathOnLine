3f3e9a0d644906f974efd992f5e6ac1b245b2503
mimis <fatal.experience@gmail.com>
2018-04-13 13:53:49 -0500

    recta por dos puntos ejemplo 3

--- application/views/tutorial/geom_ana/rectas_ecu_lineales/recta_por_dos_puntos/diapositiva_29_v.php
+++ application/views/tutorial/geom_ana/rectas_ecu_lineales/recta_por_dos_puntos/diapositiva_29_v.php
@@ -1,7 +1,7 @@
 <script src="https://cdn.geogebra.org/apps/deployggb.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>statics/js/tutorial/geom_ana/rectas _ecu_lineales/rectas_ecuaciones_lineales.js"></script>
 <script type="text/javascript" charset="utf-8">
-	var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/recta_por_dos_puntos/diapositiva_27.ggb","showToolbar":false, height: 300	}, true);
+	var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/recta_por_dos_puntos/diapositiva_29.ggb","showToolbar":false, height: 300	}, true);
 	window.addEventListener("load", function() { applet0.inject('applet_container0'); });
 </script>
 
@@ -44,6 +44,12 @@
 						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" align="left">
 							<div class="pregunta paso_0">
 								<p><b>¿Cómo es la ecuación de una recta que no pasa por el origen?</b></p>
+							</div><br>
+							<div class="paso_1">
+								<p>Por ejemplo la recta que pasa por los puntos <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>Q</mi><mo>(</mo><mo>&#xA0;</mo><mn>3</mn><mo>,</mo><mn>3</mn><mo>&#xA0;</mo><mo>)</mo></math> y <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>&#xA0;</mo><mi>R</mi><mo>(</mo><mo>&#xA0;</mo><mn>5</mn><mo>,</mo><mn>6</mn><mo>&#xA0;</mo><mo>)</mo></math></p>
+							</div><br>
+							<div class="paso_2">
+								<p>Tomemos un punto <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>P</mi><mo>(</mo><mo>&#xA0;</mo><mi>x</mi><mo>,</mo><mi>y</mi><mo>&#xA0;</mo><mo>)</mo></math> cualquiera sobre la recta. Arrastra P y observa cómo cambian sus coordenadas. <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>P</mi><mo>(</mo><mo>&#xA0;</mo><mn>5</mn><mo>,</mo><mo>&#xA0;</mo><mn>6</mn><mo>&#xA0;</mo><mo>)</mo></math></p>
 							</div><br>
 						</div>
 						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" align="center" style="border: 1px solid;">
---
+++ application/views/tutorial/geom_ana/rectas_ecu_lineales/recta_por_dos_puntos/diapositiva_39_v.php
@@ -0,0 +1,59 @@
+<script src="https://cdn.geogebra.org/apps/deployggb.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>statics/js/tutorial/geom_ana/rectas _ecu_lineales/rectas_ecuaciones_lineales.js"></script>
+<script type="text/javascript" charset="utf-8">
+	var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/recta_por_dos_puntos/diapositiva_39.ggb","showToolbar":false, height: 300	}, true);
+	window.addEventListener("load", function() { applet0.inject('applet_container0'); });
+</script>
+
+
+<div class="container-fluid">
+	<style type="text/css">
+		.tema{
+			color: #224EB5;
+			text-align: justify;
+			font-weight: bold;
+		}
+		.pregunta{
+			color: #2e75b6;
+			text-align: justify;
+			font-weight: bold;
+		}
+		.instruccion{
+			color: #000000;
+			text-align: justify;
+			font-weight: normal;
+		}
+	</style>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
+		<div class="tema"><p>La recta - Recta que pasa por dos puntos.</p>
+		</div>
+		<div class="instruccion">
+			<p>	En este tutorial aprenderás cómo encontrar la ecuación de la recta que pasa por dos puntos.
+			<br>
+			Lee con atención cada paso de las explicaciones y dedícale suficiente tiempo a los ejercicios hasta que domines el tema.<br></p>
+		</div>
+		<div id="boxEjemplos_1">
+			<div id="box_1_ejemplo_0">
+				<div class="pregunta">
+				</div>
+				<div class="instruccion">
+				</div>
+				<div class="row">
+					<div align="left"><p><label></label></p></div>
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" align="left">
+							<div class="paso_12">
+								<p>Si en <math xmlns="http://www.w3.org/1998/Math/MathML"><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mfrac mathcolor="#191919"><mrow><mi mathcolor="#A442D1">y</mi><mo mathcolor="#A442D1">-</mo><mn mathcolor="#A442D1">3</mn></mrow><mrow><mi mathcolor="#E5771D">x</mi><mo mathcolor="#E5771D">-</mo><mn mathcolor="#E5771D">3</mn></mrow></mfrac><mo mathcolor="#191919">=</mo><mfrac mathcolor="#191919"><mrow><mn mathcolor="#0000FF">6</mn><mo mathcolor="#0000FF">-</mo><mn mathcolor="#0000FF">3</mn></mrow><mrow><mn mathcolor="#FF00FF">5</mn><mo mathcolor="#FF00FF">-</mo><mn mathcolor="#FF00FF">3</mn></mrow></mfrac><mo mathcolor="#191919">=</mo><mfrac mathcolor="#191919"><mn mathcolor="#0000FF">3</mn><mn mathcolor="#FF00FF">2</mn></mfrac><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo></math> <br> despejamos y, obtenemos: <br> <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>-</mo><mn>3</mn><mo>=</mo><mfrac><mn>3</mn><mn>2</mn></mfrac><mo>(</mo><mi>x</mi><mo>-</mo><mn>3</mn><mo>)</mo></math> <br> <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><mfrac><mn>3</mn><mn>2</mn></mfrac><mo>(</mo><mi>x</mi><mo>-</mo><mn>3</mn><mo>)</mo><mo>+</mo><mn>3</mn></math><br> <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><mfrac><mn>3</mn><mn>2</mn></mfrac><mi>x</mi><mo>-</mo><mfrac><mn>9</mn><mn>2</mn></mfrac><mo>+</mo><mn>3</mn></math> <br> <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>y</mi><mo>=</mo><mfrac><mn>3</mn><mn>2</mn></mfrac><mi>x</mi><mo>-</mo><mfrac><mn>3</mn><mn>2</mn></mfrac></math> </p>
+								<p>Ésta es la ecuación de la recta que pasa por los puntos <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>Q</mi><mo>(</mo><mo>&#xA0;</mo><mn>3</mn><mo>,</mo><mn>3</mn><mo>&#xA0;</mo><mo>)</mo></math> y <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>R</mi><mo>(</mo><mo>&#xA0;</mo><mn>5</mn><mo>,</mo><mn>6</mn><mo>&#xA0;</mo><mo>)</mo></math>.</p>
+							</div><br>
+						</div>
+						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" align="center" style="border: 1px solid;">
+							<div style="display: block" align="center" class="col-xs-12 col-sm-12 col-lg-12" id="applet_container0"></div>
+						</div>
+					</div>											
+				</div><br>
+			</div><br><br>
+		</div>
+	</div>
+	<br><br>
+  </div>
