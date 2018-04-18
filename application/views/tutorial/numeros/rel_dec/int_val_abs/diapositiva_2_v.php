af2387234f421eb09ef1899599fbd841e6b4be7a
krmncc <carmen.chavez.conde@gmail.com>
2018-04-17 22:19:42 -0500

    segundo avance 17/04/2018

--- application/views/tutorial/numeros/rel_dec/int_val_abs/diapositiva_2_v.php
+++ application/views/tutorial/numeros/rel_dec/int_val_abs/diapositiva_2_v.php
@@ -1,26 +1,34 @@
 <script src="<?php echo base_url(); ?>statics/js/tutorial/numeros/rel_dec/int_val_abs.js"></script>
 
 <div class="container-fluid">
-	
-		
-		<div  align="center" class=" col-lg-12 col-md-12  col-xs-12">
+	<div  align="center" class=" col-lg-12 col-md-12  col-xs-12">
 			<p align="justify">¿Cómo se miden las distancias entre dospuntos en la recta numérica?¿Què caracterìsticas que debe tener la forma de medir la distancia?</p>
 			<p align="left">Entre las afirmaciones siguientes elige aquella que NO debe ser una característica de la forma de medir la distancia entre dos puntos:</p>
 			<div  align="left" class=" col-lg-12 col-md-12  col-xs-12">
-			Estima la cantidad antes de resolver el problema:
-		   	<li>Menos de 1 litro de agua.</li> 
-		   	<li>Más de 1 litro de agua pero menos de 2 litros de agua.</li> 
-			<li>Más de 2 litros de agua.</li> 
-			<li>Más de tres litros de agua.</li> 
-		</div>	
+				<ol>
+		   		<li>Si dos puntos se encuentran en posciones diferentes opuestas al origen, su distancia al origen debe ser la misma sin importar que se encuentren de un lado o del otro del origen.</li>
+		   		<li>La distancia obtenida debe ser no negativa.</li>
+					<li>La distancia entre dos puntos A y B debe ser la misma si se divide de A a B o si se mide de B a A.</li>
+					<li>La distancia entre dos puntos puede ser positiva o negativa.</li>
+				</ol>
+			</div>
+			<div class="dropdown" align="left">
+    		<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Opciones:
+    		<span class="caret"></span></button>
+    		<ul class="dropdown-menu">
+      		<li id="r1"><a href="#">1</a></li>
+      		<li id="r2"><a href="#">2</a></li>
+      		<li id="r3"><a href="#">3</a></li>
+					<li id="r4"><a href="#">4</a></li>
+    		</ul>
+  		</div>
 			<table>
 					<tr align="center">
-						<td><h1><math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mn>1</mn><mn>2</mn></mfrac></math></h1></div></td>
-						<td><img width="50%" class="img-responsive" src="<?php echo base_url(); ?>statics/img/tutorial/numeros/fracc_pct_rzns_pro/lata_un_medio.png"  /></div></td>
+
 					</tr>
 			</table>
-		</div>		
-		
-		
-		
+		</div>
+
+
+
 </div>
