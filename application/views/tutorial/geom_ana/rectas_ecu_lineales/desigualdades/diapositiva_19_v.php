9e2f8e771a08026ce8552b8d441cba7eccab94c3
mimis <fatal.experience@gmail.com>
2018-05-07 13:25:07 -0500

    ejemplos desigualdades

--- application/views/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_19_v.php
+++ application/views/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_19_v.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>statics/css/tutorial/geom_ana/rectas _ecu_lineales/rectas_ecuaciones_lineales.css">
 
 <script type="text/javascript" charset="utf-8">
-    var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_15.ggb","showToolbar":false, height: 100  }, true);
+    var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_19.ggb","showToolbar":false, height: 100  }, true);
     window.addEventListener("load", function() { applet0.inject('applet_container0'); });
 </script>
 
@@ -28,6 +28,9 @@
                             <div class="paso_0">
                                 <p>Si sabes que <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>x</mi><mo>&#x2265;</mo><mo>-</mo><mn>4</mn></math>, ¿Qué valores puede tomar <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><mi>x</mi></math>?</p>
                                 <p>Veamos lo que sucede al multiplicar la desigualdad <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>x</mi><mo>&#x2265;</mo><mo>-</mo><mn>4</mn></math> por <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><mn>1</mn></math></p>
+                            </div><br>
+                            <div class="paso_4">
+                                <p>Para <math xmlns="http://www.w3.org/1998/Math/MathML"><mo mathcolor="#007F00">&#xA0;</mo><mi mathcolor="#191919">x</mi><mo mathcolor="#191919">=</mo><mn mathcolor="#191919">8</mn><mo mathcolor="#191919">.</mo><mn mathcolor="#191919">8</mn><mo mathcolor="#191919">&#xA0;</mo></math> tenemos: <math xmlns="http://www.w3.org/1998/Math/MathML"><mo mathcolor="#007F00">&#xA0;</mo><mn mathcolor="#FF0000">8</mn><mo mathcolor="#FF0000">.</mo><mn mathcolor="#FF0000">8</mn><mo mathcolor="#007F00">&#xA0;</mo><mo mathcolor="#333333">&#xD7;</mo><mo mathcolor="#333333">&#xA0;</mo><mo mathcolor="#333333">(</mo><mo mathcolor="#333333">&#xA0;</mo><mo mathcolor="#333333">-</mo><mn mathcolor="#333333">1</mn><mo mathcolor="#333333">&#xA0;</mo><mo mathcolor="#333333">)</mo><mo mathcolor="#333333">&#xA0;</mo><mo mathcolor="#333333">=</mo><mo mathcolor="#007F00">&#xA0;</mo><mo mathcolor="#007F00">-</mo><mn mathcolor="#007F00">8</mn><mo mathcolor="#007F00">.</mo><mn mathcolor="#007F00">8</mn><mo mathcolor="#007F00">&#xA0;</mo><mo mathcolor="#007F00">&#xA0;</mo></math></p>
                             </div><br>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" align="center" style="border: 1px solid;">
--- application/views/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_3_v.php
+++ application/views/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_3_v.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>statics/css/tutorial/geom_ana/rectas _ecu_lineales/rectas_ecuaciones_lineales.css">
 
 <script type="text/javascript" charset="utf-8">
-    var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_8.ggb","showToolbar":false, height: 100  }, true);
+    var applet0 = new GGBApplet({filename: base_url+"statics/ggb/tutorial/geom_ana/rectas_ecu_lineales/desigualdades/diapositiva_3.ggb","showToolbar":false, height: 100  }, true);
     window.addEventListener("load", function() { applet0.inject('applet_container0'); });
 </script>
 
@@ -22,12 +22,15 @@
                 <p></p>
                 </div>
                 <div class="row">
-                    <div align="left"><p><label>Veamos otro ejemplo</label></p></div>
+                    <div align="left"><p><label>Ejemplos</label></p></div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" align="left">
                             <div class="paso_0">
-                                <p>Si sabes que <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>x</mi><mo>&lt;</mo><mo>-</mo><mn>2</mn></math>, ¿Qué valores puede tomar <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><mi>x</mi></math>?</p>
-                                <p>Veamos lo que sucede al multiplicar la desigualdad <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>x</mi><mo>&lt;</mo><mo>-</mo><mn>2</mn></math> por <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><mn>1</mn></math></p>
+                                <p>Si sabes que <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>x</mi><mo>&#x2265;</mo><mn>5</mn></math>, ¿Qué valores puede tomar <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><mi>x</mi></math>?</p>
+                                <p>Veamos lo que sucede al multiplicar la desigualdad <math xmlns="http://www.w3.org/1998/Math/MathML"><mi>x</mi><mo>&#x2265;</mo><mn>5</mn></math> por <math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><mn>1</mn></math></p>
+                            </div><br>
+                            <div class="paso_2">
+                                <p>Para <math xmlns="http://www.w3.org/1998/Math/MathML"><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo><mi mathcolor="#191919">x</mi><mo mathcolor="#191919">=</mo><mn mathcolor="#191919">7</mn><mo mathcolor="#191919">.</mo><mn mathcolor="#191919">5</mn><mo mathcolor="#191919">&#xA0;</mo><mo mathcolor="#191919">&#xA0;</mo></math> tenemos: <math xmlns="http://www.w3.org/1998/Math/MathML"><mo mathcolor="#FF0000">&#xA0;</mo><mn mathcolor="#FF0000">7</mn><mo mathcolor="#FF0000">.</mo><mo mathcolor="#FF0000">&#xA0;</mo><mn mathcolor="#FF0000">5</mn><mo mathcolor="#FF0000">&#xA0;</mo><mo>&#xD7;</mo><mo>&#xA0;</mo><mo>(</mo><mo>-</mo><mn>1</mn><mo>)</mo><mo>&#xA0;</mo><mo>=</mo><mo>&#xA0;</mo><mo mathcolor="#007F7F">-</mo><mn mathcolor="#007F7F">7</mn><mo mathcolor="#007F7F">.</mo><mn mathcolor="#007F7F">5</mn><mo mathcolor="#007F7F">&#xA0;</mo></math></p>
                             </div><br>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" align="center" style="border: 1px solid;">
